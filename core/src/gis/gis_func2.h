11e336da3d3891ef5204bd34b1e723411953bcc3
xiaocai <1170613300@qq.com>
2020-01-10 14:50:48 +0800

    multi cpp to one module

--- core/src/gis/gis_func2.h
+++ core/src/gis/gis_func2.h
@@ -1,8 +1,16 @@
-//
-// Created by zc on 20-1-10.
-//
 
 #ifndef GIS_GIS_FUNC2_H
 #define GIS_GIS_FUNC2_H
 
+#include "arrow/api.h"
+
+
+using namespace std;
+
+namespace gis {
+
+shared_ptr<arrow::Array>
+gis_func2(shared_ptr<arrow::Array> arr_ptr1, shared_ptr<arrow::Array> arr_ptr2);
+
+}
 #endif //GIS_GIS_FUNC2_H
