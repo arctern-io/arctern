0b808a61c5f7b96b5e8850b2c39eda38f7b24f07
jiquan.long <jiquan.long@zilliz.com>
2020-01-13 18:17:13 +0800

    resolve dependency: rapidjson

---
+++ core/cmake/ConfigureRapidJson.cmake
@@ -0,0 +1,24 @@
+set(RAPIDJSON_SOURCE_URL
+    "https://github.com/Tencent/rapidjson"
+)
+
+set(RAPIDJSON_ROOT      ${CMAKE_BINARY_DIR}/rapidjson_ep-prefix/src)
+file(MAKE_DIRECTORY     ${RAPIDJSON_ROOT}/rapidjson-build)
+ExternalProject_Add(rapidjson_ep
+    GIT_REPOSITORY
+    ${RAPIDJSON_SOURCE_URL}
+    GIT_TAG
+    master
+    BINARY_DIR
+    ${RAPIDJSON_ROOT}/rapidjson-build
+    CONFIGURE_COMMAND
+    cmake
+    -DCMAKE_BUILD_TYPE=Release
+    -DRAPIDJSON_BUILD_TESTS=OFF
+    -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/thirdparty
+    ${RAPIDJSON_ROOT}/rapidjson_ep
+    BUILD_COMMAND
+    make
+    INSTALL_COMMAND
+    make install
+)
