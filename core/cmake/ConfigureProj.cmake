b785a021920dead1107e4af8e90049097553c3b8
jiquan.long <jiquan.long@zilliz.com>
2020-01-13 12:13:55 +0800

    resolve dependency: proj4

--- core/cmake/ConfigureProj.cmake
+++ core/cmake/ConfigureProj.cmake
@@ -1,18 +1,34 @@
-set(PROJ_ROOT   ${CMAKE_BINARY_DIR}/proj)
+set(PROJ_SOURCE_URL "https://download.osgeo.org/proj/proj-6.2.1.tar.gz")
 
-# note that sqlite3 must be built before this external project 
- set(PROJ_CMAKE_ARGS        " -DCMAKE_PREFIX_PATH=${CMAKE_BINARY_DIR}/thirdpart"
-                            " -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/thirdparty"
-                            " -DSQLITE3_INCLUDE_DIR=${CMAKE_BINARY_DIR}/thirdparty/include"
-                            " -DSQLITE3_LIBRARY=${CMAKE_BINARY_DIR}/thirdparty/lib/libsqlite3.so"
-                            " -DEXE_SQLITE3=${CMAKE_BINARY_DIR}/thirdparty/bin/sqlite3"
-                            " -DPROJ_TESTS=OFF")
+set(PROJ_ROOT   ${CMAKE_BINARY_DIR}/proj_ep-prefix/src)
 
-set(PROJ_SOURCE_URL
-        "https://download.osgeo.org/proj/proj-6.2.1.tar.gz")
+# don't work in configure command
+string(CONCAT PROJ_CMAKE_ARGS       " -DCMAKE_PREFIX_PATH=${CMAKE_BINARY_DIR}/thirdpart"
+                                    " -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/thirdparty"
+                                    " -DSQLITE3_INCLUDE_DIR=${CMAKE_BINARY_DIR}/thirdparty/include"
+                                    " -DSQLITE3_LIBRARY=${CMAKE_BINARY_DIR}/thirdparty/lib/libsqlite3.so"
+                                    " -DEXE_SQLITE3=${CMAKE_BINARY_DIR}/thirdparty/bin/sqlite3"
+                                    " -DPROJ_TESTS=OFF")
 
+file(MAKE_DIRECTORY     ${PROJ_ROOT}/proj-build)
 ExternalProject_Add(proj_ep
-                    URL                 ${PROJ_SOURCE_URL}
-                    CMAKE_ARGS          ${PROJ_CMAKE_ARGS})
-                
+    URL
+    ${PROJ_SOURCE_URL}
+    BINARY_DIR
+    ${PROJ_ROOT}/proj-build
+    CONFIGURE_COMMAND
+    cmake
+    -DCMAKE_PREFIX_PATH=${CMAKE_BINARY_DIR}/thirdparty
+    -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/thirdparty
+    -DSQLITE3_INCLUDE_DIR=${CMAKE_BINARY_DIR}/thirdparty/include
+    -DSQLITE3_LIBRARY=${CMAKE_BINARY_DIR}/thirdparty/lib/libsqlite3.so
+    -DEXE_SQLITE3=${CMAKE_BINARY_DIR}/thirdparty/bin/sqlite3
+    -DPROJ_TESTS=OFF
+    ${PROJ_ROOT}/proj_ep
+    BUILD_COMMAND
+    make
+    INSTALL_COMMAND
+    make install
+)
+
 add_dependencies(proj_ep sqlite_ep)
