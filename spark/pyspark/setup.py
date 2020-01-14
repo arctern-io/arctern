73d6f53b8a1d2415967ef9136f7c02051d4d0502
zhenshan.cao <zhenshan.cao@zilliz.com>
2020-01-14 17:09:11 +0800

    simplify setup.py for pyspark

--- spark/pyspark/setup.py
+++ spark/pyspark/setup.py
@@ -1,14 +1,8 @@
-from distutils.core import setup, Extension
+from distutils.core import setup
 import os
-import numpy as np
-import pyarrow as pa
-
-# MOD = "zilliz_gis"
 
 real_path = os.path.realpath(__file__)
 dir_path = os.path.dirname(real_path)
-
-from distutils.core import setup
 
 setup(
     name = "zilliz_pyspark",
