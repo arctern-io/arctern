5a334f9f43289f0f34654f19b887abbc1786f91d
jiquan.long <jiquan.long@zilliz.com>
2020-01-14 11:46:55 +0800

    fix build directory bug

--- core/cmake/ConfigureArrow.cmake
+++ core/cmake/ConfigureArrow.cmake
@@ -7,8 +7,12 @@ set(ARROW_CMAKE_ARGS    " -DARROW_BUILD_BENCHMARKS=OFF"
                         " -DARROW_BUILD_TESTS=OFF"
                         " -DARROW_PYTHON=ON")
 
+file(MAKE_DIRECTORY "${ARROW_ROOT}/build")
+
 ExternalProject_Add(arrow_ep
                     GIT_REPOSITORY      https://github.com/apache/arrow.git
                     GIT_TAG             apache-arrow-0.15.1
+                    SOURCE_DIR          "${ARROW_ROOT}/arrow"
                     SOURCE_SUBDIR       "cpp"
+                    BINARY_DIR          "${ARROW_ROOT}/build"
                     CMAKE_ARGS          ${ARROW_CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/thirdparty)
