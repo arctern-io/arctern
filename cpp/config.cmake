213ec3ecd821ef70afa16793b29b4cf18e8a7ba9
xi.ge <xi.ge@zilliz.com>
2020-01-18 18:04:17 +0800

    add /GIS/cpp/api.h

--- cpp/config.cmake
+++ cpp/config.cmake
@@ -1,5 +1,10 @@
 set(USE_GPU on)
-set(CMAKE_BUILD_TYPE Debug)
-set(BUILD_UNITTEST on)
+
+#set(CMAKE_BUILD_TYPE Debug)
+
+#set(BUILD_UNITTEST on)
+
 #set(CMAKE_CUDA_COMPILER /usr/local/cuda/bin/nvcc)
-set(PYTHON_EXECUTABLE /home/xge/anaconda2/envs/GIS/bin/python)
+
+# set python_executable path to python envs with pyarrow installed
+#set(PYTHON_EXECUTABLE ./anaconda2/envs/GIS/bin/python)
