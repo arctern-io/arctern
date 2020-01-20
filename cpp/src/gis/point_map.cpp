b0cd1d5c157ea5724066651b9075eeb1134a021e
jiquan.long <jiquan.long@zilliz.com>
2020-01-20 17:35:30 +0800

    nothing

--- cpp/src/gis/point_map.cpp
+++ cpp/src/gis/point_map.cpp
@@ -19,21 +19,5 @@
 std::shared_ptr<arrow::Array>
 point_map(std::shared_ptr<arrow::Array> arr_x,
            std::shared_ptr<arrow::Array> arr_y) {
-
-    auto d_arr_x = std::static_pointer_cast<arrow::DoubleArray>(arr_x);
-    auto d_arr_y = std::static_pointer_cast<arrow::DoubleArray>(arr_y);
-
-    std::shared_ptr<arrow::Array> point_arr;
-
-
-
-    // TODO: use cuda parallesim to improve this
-    int64_t length = arr_x->length();
-    assert(length == arr_y->length());
-
-    arrow::StringBuilder builder;
-    builder.Append("point_map");
-    CHECK_STATUS(builder.Finish(&point_arr));
-
-    return point_arr;
+    return render
 }
