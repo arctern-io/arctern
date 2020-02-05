56837f5ceec7ae51929beb4d912e7f263aa9fbea
yefu.chen <yefu.chen@zilliz.com>
2020-02-05 15:12:41 +0800

    gdal

--- cpp/cmake/ConfigureGdal.cmake
+++ cpp/cmake/ConfigureGdal.cmake
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-#set(GDAL_SOURCE_URL
-#    "https://github.com/OSGeo/gdal/releases/download/v3.0.2/gdal-3.0.2.tar.gz")
-#
-#set(GDAL_ROOT       ${CMAKE_BINARY_DIR}/gdal_ep-prefix/src/)
-#
-#ExternalProject_Add(gdal_ep
-#    URL
-#    ${GDAL_SOURCE_URL}
-#    URL_MD5
-#    8a31507806b26f070858558aaad42277
-#    CONFIGURE_COMMAND
-#    ./configure
-#    CPPFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0
-#    --prefix=${CMAKE_BINARY_DIR}/thirdparty
-#    --with-proj=${CMAKE_BINARY_DIR}/thirdparty
-#    --without-libtool
-#    --with-zstd=no
-#    --with-cryptopp=no
-#    --with-expat=no
-#    --with-crypto=no
-#    BUILD_COMMAND
-#    make -j8
-#    INSTALL_COMMAND
-#    make install
-#    BUILD_IN_SOURCE
-#    1
-#)
-
-=======
->>>>>>> f395151f99a27e82918bd1e34a7b405f239481c7
 set(GDAL_SOURCE_URL
         "https://github.com/OSGeo/gdal.git")
 set(GDAL_ROOT       ${CMAKE_BINARY_DIR}/gdal_ep-prefix/src/)
@@ -58,11 +27,6 @@ ExternalProject_Add(gdal_ep
         make -j8
         INSTALL_COMMAND
         make install
-<<<<<<< HEAD
-#        BUILD_IN_SOURCE
-#        1
-=======
->>>>>>> f395151f99a27e82918bd1e34a7b405f239481c7
         )
 
 add_dependencies(gdal_ep proj_ep)
