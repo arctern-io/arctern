b1f68e3e7100ae9fec08af8ca49448df7ae22163
fluorinedog <fluorinedog@gmail.com>
2020-02-07 18:53:13 +0800

    update

--- cuda/src/wkb_decode.cpp
+++ cuda/src/wkb_decode.cpp
@@ -33,7 +33,7 @@ GeometryVector::decodeFromWKB(const char* raw_bin) {
     auto byte_order = fetch<WKB_ByteOrder>(stream);
     assert(byte_order == WKB_ByteOrder::LittleEndian);
     auto tag = fetch<WKB_Tag>(stream);
-
+    return tmp;
 }
 
 }    // namespace cpp
