b79cdd6d5ecf85ce07f38682db67e954e98c15e2
fluorinedog <fluorinedog@gmail.com>
2020-02-10 22:29:25 +0800

    wtf

---
+++ cuda/unittest/FunctorDistance_tests.cpp
@@ -0,0 +1,15 @@
+//
+// Created by mike on 2/10/20.
+//
+
+#include <gtest/gtest.h>
+
+TEST(FunctorDistance, naive) {
+    ASSERT_TRUE(true);
+    char data[1 + 4 + 16];
+    int num = 5;
+
+    for(int i = 0; i < num; ++i) {
+        
+    }
+}
