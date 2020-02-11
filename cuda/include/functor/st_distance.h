5f0457759261d62a6aac7bb2692f6cc3373cd11e
fluorinedog <fluorinedog@gmail.com>
2020-02-11 14:21:16 +0800

    updat

--- cuda/include/functor/st_distance.h
+++ cuda/include/functor/st_distance.h
@@ -5,7 +5,7 @@ namespace zilliz {
 namespace gis {
 namespace cpp {
 
-void ST_distance(const GeometryVector& left, const GeometryVector& right, double* result);
+void ST_distance(const GeometryVector& left, const GeometryVector& right, double* host_results);
 
 }    // namespace cpp
 }    // namespace gis
