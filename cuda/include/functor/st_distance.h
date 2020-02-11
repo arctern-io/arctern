f1a18e3b469aefc9cede8a75bfcccdaebde6e044
fluorinedog <fluorinedog@gmail.com>
2020-02-11 13:35:26 +0800

    st_distance

---
+++ cuda/include/functor/st_distance.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "common/gis_definitions.h"
+
+namespace zilliz {
+namespace gis {
+namespace cpp {
+
+void ST_distance(const GeometryVector& left, const GeometryVector& right, double* result);
+
+}    // namespace cpp
+}    // namespace gis
+}    // namespace zilliz
--- cuda/src/functor/st_distance.cu
+++ cuda/src/functor/st_distance.cu
@@ -1,10 +1,10 @@
 //
 // Created by mike on 2/10/20.
 //
-#include "common/gis_definitions.h"
 #include <cuda_runtime.h>
 #include <cuda_device_runtime_api.h>
 #include <cmath>
+#include "functor/st_distance.h"
 
 namespace zilliz {
 namespace gis {
@@ -12,8 +12,8 @@ namespace cpp {
 
 using GeoContext = GeometryVector::GPUContext;
 
-inline
-DEVICE_RUNNABLE double ST_distance_point_point(const GeoContext& left, const GeoContext& right, int index) {
+inline DEVICE_RUNNABLE double
+ST_distance_point_point(const GeoContext& left, const GeoContext& right, int index) {
     auto lv = left.get_value_ptr(index);
     auto rv = right.get_value_ptr(index);
     auto dx = (lv[0] - rv[0]);
@@ -21,16 +21,17 @@ DEVICE_RUNNABLE double ST_distance_point_point(const GeoContext& left, const Geo
     return sqrt(dx * dx + dy * dy);
 }
 
-__global__
-void ST_distance_kernel(GeoContext left, GeoContext right, double* result) {
+__global__ void
+ST_distance_kernel(GeoContext left, GeoContext right, double* result) {
     auto tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if(tid < left.size) {
+    if (tid < left.size) {
         auto left_tag = left.get_tag(tid);
         auto right_tag = right.get_tag(tid);
         // handle 2d case only for now
         assert(left_tag.get_group() == WKB_Group::None);
         assert(right_tag.get_group() == WKB_Group::None);
-        if(left_tag.get_category() == WKB_Category::Point && right_tag.get_category() == WKB_Category::Point) {
+        if (left_tag.get_category() == WKB_Category::Point &&
+            right_tag.get_category() == WKB_Category::Point) {
             result[tid] = ST_distance_point_point(left, right, tid);
         } else {
             result[tid] = NAN;
@@ -43,7 +44,9 @@ ST_distance(const GeometryVector& left, const GeometryVector& right, double* res
     assert(left.size() == right.size());
     auto left_ctx = left.create_gpuctx();
     auto right_ctx = right.create_gpuctx();
-//    ST_distance_kernel<<<>>>()
+    auto config = GetKernelExecConfig(left.size());
+    ST_distance_kernel<<<config.grid_dim, config.block_dim>>>(
+        left_ctx.get(), right_ctx.get(), result);
 }
 
 }    // namespace cpp
