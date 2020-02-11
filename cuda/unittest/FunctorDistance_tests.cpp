eae0801b0821139b67f6ba9d4ab37c31c9ecbf12
fluorinedog <fluorinedog@gmail.com>
2020-02-11 16:04:09 +0800

    udpate

--- cuda/unittest/FunctorDistance_tests.cpp
+++ cuda/unittest/FunctorDistance_tests.cpp
@@ -15,9 +15,11 @@ using namespace zilliz::gis::cpp;
 TEST(FunctorDistance, naive) {
     ASSERT_TRUE(true);
     // TODO use gdal to convert better good
+
     // POINT(3 1), copy from WKB WKT convertor
-//    uint8_t data_left[] = {x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-//                      0x08, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x3f};
+    //    uint8_t data_left[] = {0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+    //                           0x00, 0x00, 0x00, 0x00, 0x08, 0x40, 0x00, 0x00, 0x00,
+    //                           0x00, 0x00, 0x00, 0xf0, 0x3f};
     auto vec_left = hexstring_to_binary("01010000000000000000000840000000000000f03f");
     auto data_left = vec_left.data();
     char data[1 + 4 + 16];
@@ -48,8 +50,8 @@ TEST(FunctorDistance, naive) {
     auto left_ctx = gvec_left.create_gpuctx();
     vector<double> result(5);
     ST_distance(gvec_left, gvec_right, result.data());
-    for(int i = 0; i < num; ++i) {
-        auto std = sqrt(pow(i - 3, 2)  + pow(i + 1 - 1, 2));
+    for (int i = 0; i < num; ++i) {
+        auto std = sqrt(pow(i - 3, 2) + pow(i + 1 - 1, 2));
         ASSERT_DOUBLE_EQ(result[i], std);
     }
 }
