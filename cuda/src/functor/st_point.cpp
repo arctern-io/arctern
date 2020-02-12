924ebe093e17b1e0e5a1d2a3ec6110b9d5ee21ed
fluorinedog <fluorinedog@gmail.com>
2020-02-13 00:28:40 +0800

    add point

--- cuda/src/functor/st_point.cpp
+++ cuda/src/functor/st_point.cpp
@@ -4,8 +4,29 @@ namespace zilliz {
 namespace gis {
 namespace cpp {
 
+// this is a slow implementation, only to reflex the real proess
+enum Action {
+    CalculateSpace,
+    WriteData
+};
+
+
+
+//__device__ int size ST_point_kernel(double* meta_output, double* value_output) {
+//
+//}
+
+
+
+__global__ void ST_point_reserve_kernel(const double* xs, const double* ys, int size, GeoContext results) {
+    auto index = threadIdx.x + blockIdx.x * blockDim.x;
+    if(index < results.size) {
+
+    }
+}
+
 void ST_point(const double* xs, const double ys, size_t size, GeometryVector& results) {
-//    results
+
 }
 
 }    // namespace cpp
