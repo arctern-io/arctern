d56da04770369904c82dfdfd9fbfed65caf92e8f
fluorinedog <fluorinedog@gmail.com>
2020-02-15 07:18:27 +0800

    rule-based

--- cuda/src/functor/st_point.cu
+++ cuda/src/functor/st_point.cu
@@ -20,39 +20,44 @@ ST_point_compute_kernel(const double* xs,
                         const double* ys,
                         int index,
                         uint32_t* meta_output,
-                        double* value_output) {
+                        double* value_output,
+                        bool skip_write = false) {
     (void)meta_output;
-    value_output[0] = xs[index];
-    value_output[1] = ys[index];
+    if (!skip_write) {
+        assert(value_output != nullptr);
+        value_output[0] = xs[index];
+        value_output[1] = ys[index];
+    }
     return OutputInfo{WkbTag(WkbCategory::Point, WkbGroup::None), 0, 2};
 }
 
 __global__ void
-ST_point_reserve_kernel(const double* xs, const double* ys, GeoContext results, GeoWorkspace space) {
+ST_point_reserve_kernel(const double* xs, const double* ys, GeoContext results) {
     assert(results.data_state == DataState::FlatOffset_EmptyInfo);
-    auto init_index = threadIdx.x + blockIdx.x * blockDim.x;
-    auto meta_buffer = results.metas + max_buffer_per_meta * init_index;
-    auto value_buffer = results.values + max_buffer_per_value * init_index;
-    for (auto index = init_index; index < results.size; index += blockDim.x * gridDim.x) {
-        auto out_info = ST_point_compute_kernel(xs, ys, index, meta_buffer, value_buffer);
+    auto index = threadIdx.x + blockIdx.x * blockDim.x;
+    if (index < results.size) {
+        auto out_info = ST_point_compute_kernel(xs, ys, index, nullptr, nullptr, true);
         results.tags[index] = out_info.tag;
         results.meta_offsets[index] = out_info.meta_size;
         results.value_offsets[index] = out_info.value_size;
     }
 }
 
-DEVICE_RUNNABLE void
+
+DEVICE_RUNNABLE inline void
 check_info(OutputInfo info, const GeoContext& ctx, int index) {
     assert(info.tag.data_ == ctx.get_tag(index).data_);
     assert(info.meta_size == ctx.meta_offsets[index + 1] - ctx.meta_offsets[index]);
     assert(info.value_size == ctx.value_offsets[index + 1] - ctx.value_offsets[index]);
 }
 
+static
 __global__ void
 ST_point_datafill_kernel(const double* xs,
                          const double* ys,
                          int size,
-                         GeoContext results) {
+                         GeoContext results,
+                         GeoWorkspace) {
     assert(results.data_state == DataState::PrefixSumOffset_EmptyData);
     auto index = threadIdx.x + blockIdx.x * blockDim.x;
     if (index < results.size) {
@@ -64,8 +69,8 @@ ST_point_datafill_kernel(const double* xs,
 }
 
 
-//GeometryVector
-//ST_point(const double* xs, const double ys, size_t size) {
+// GeometryVector
+// ST_point(const double* xs, const double ys, size_t size) {
 //
 //}
 
