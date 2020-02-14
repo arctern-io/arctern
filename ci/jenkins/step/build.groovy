c8dfb227d930f7eb584a24c841e947e0dfff23a3
quicksilver <zhifeng.zhang@zilliz.com>
2020-02-14 10:38:03 +0800

    format build.groovy

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -1,9 +1,9 @@
 timeout(time: 15, unit: 'MINUTES') {
     dir ("ci/scripts") {
         if ("${BINARY_VERSION}" == "gpu") {
-            sh "./cpp_build.sh -t ${params.BUILD_TYPE} -o ${env.GIS_INSTALL_PREFIX} -e \"zgis_dev\" -g -u"
+            sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -o ${env.GIS_INSTALL_PREFIX} -e \"zgis_dev\" -g -u'"
         } else {
-            sh "./cpp_build.sh -t ${params.BUILD_TYPE} -o ${env.GIS_INSTALL_PREFIX} -e \"zgis_dev\" -u"
+            sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -o ${env.GIS_INSTALL_PREFIX} -e \"zgis_dev\" -u'"
         }
     }
 }
