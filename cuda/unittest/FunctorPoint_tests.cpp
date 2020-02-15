8ad2ad3691bbc2aae7d43ac4bca5a995f087ea4d
fluorinedog <fluorinedog@gmail.com>
2020-02-15 17:43:58 +0800

    final passed

--- cuda/unittest/FunctorPoint_tests.cpp
+++ cuda/unittest/FunctorPoint_tests.cpp
@@ -6,6 +6,7 @@
 #include "gis/gis_definitions.h"
 #include "functor/st_point.h"
 #include <cmath>
+#include <functor/st_distance.h>
 #include "test_helper.h"
 
 using namespace zilliz;
@@ -15,7 +16,21 @@ using namespace zilliz::gis::cuda;
 TEST(FunctorPoint, naive) {
     vector<double> xs{1, 2, 3, 4, 5};
     vector<double> ys{0, 1, 2, 3, 4};
-    GeometryVector result;
-    ST_point(xs.data(), ys.data(), xs.size(), result);
-    auto x = 1 + 1;
+    GeometryVector left_points;
+    GeometryVector right_points;
+    ST_point(xs.data(), ys.data(), xs.size(), left_points);
+    for(auto &x: xs) {
+        x = -x;
+    }
+    for(auto &y: ys) {
+        y = -y;
+    }
+    ST_point(xs.data(), ys.data(), xs.size(), right_points);
+    vector<double> distance(xs.size());
+    ST_distance(left_points, right_points, distance.data());
+    for(int i = 0; i < xs.size(); ++i) {
+        auto std = (xs[i] * xs[i] + ys[i] * ys[i]) * 4;
+        auto res = distance[i] * distance[i];
+        ASSERT_DOUBLE_EQ(res, std);
+    }
 }
