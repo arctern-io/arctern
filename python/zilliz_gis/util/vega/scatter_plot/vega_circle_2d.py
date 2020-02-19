92a20e955b3329e57e024bc30cc3f68df2aee23c
zhenshan.cao <zhenshan.cao@zilliz.com>
2020-02-19 16:24:19 +0800

    format python code

--- python/zilliz_gis/util/vega/scatter_plot/vega_circle_2d.py
+++ python/zilliz_gis/util/vega/scatter_plot/vega_circle_2d.py
@@ -1,12 +1,27 @@
-from zilliz_gis.util.vega.scatter_plot.vega_scatter_plot import VegaScatterPlot
-from zilliz_gis.util.vega.vega_node import *
+# Copyright (C) 2019-2020 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import json
+from zilliz_gis.util.vega.scatter_plot.vega_scatter_plot import VegaScatterPlot
+from zilliz_gis.util.vega.vega_node import (RootMarks, Root, Description, Data,
+                                            Width, Height, Scales)
 
-"""
-Top-Level Vega Specification Property: Marks
-"""
 class Marks(RootMarks):
+    """
+        Top-Level Vega Specification Property: Marks
+    """
+
     class Encode:
         class Value:
             def __init__(self, v: int or float or str):
@@ -19,8 +34,8 @@ class Marks(RootMarks):
                 return dic
 
         def __init__(self, shape: Value, stroke: Value, strokeWidth: Value, opacity: Value):
-            if not (type(shape.v) == str and type(strokeWidth.v) == int and
-                    type(stroke.v) == str and type(opacity.v) == float):
+            if not (isinstance(shape.v, str) and isinstance(strokeWidth.v, int) and
+                    isinstance(stroke.v, str) and isinstance(opacity.v, float)):
                 # TODO error log here
                 print("illegal")
                 assert 0
@@ -61,16 +76,16 @@ class VegaCircle2d(VegaScatterPlot):
         data = Data(name="data", url="/data/data.csv")
         domain1 = Scales.Scale.Domain(data="data", field="c0")
         domain2 = Scales.Scale.Domain(data="data", field="c1")
-        scale1 = Scales.Scale(name="x", type="linear", domain=domain1)
-        scale2 = Scales.Scale(name="y", type="linear", domain=domain2)
+        scale1 = Scales.Scale("x", "linear", domain1)
+        scale2 = Scales.Scale("y", "linear", domain2)
         scales = Scales([scale1, scale2])
         encode = Marks.Encode(shape=Marks.Encode.Value("circle"),
                               stroke=Marks.Encode.Value(self._mark_color),
                               strokeWidth=Marks.Encode.Value(self._mark_size),
                               opacity=Marks.Encode.Value(self._opacity))
         marks = Marks(encode)
-        root = Root(width=Width(self._width), height=Height(self._height), description=description,
-                    data=data, scales=scales, marks=marks)
+        root = Root(Width(self._width), Height(self._height), description,
+                    data, scales, marks)
 
         root_json = json.dumps(root.to_dict(), indent=2)
         return root_json
