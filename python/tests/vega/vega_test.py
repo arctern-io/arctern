4c8e9ca5005ba7839bff1631a293c716d8c8fcd8
bowen <954206947@qq.com>
2020-02-20 14:53:35 +0800

    add vega choropleth map and vega pytest

--- python/tests/vega/vega_test.py
+++ python/tests/vega/vega_test.py
@@ -15,6 +15,7 @@
 import json
 from zilliz_gis.util.vega.scatter_plot.vega_circle_2d import VegaCircle2d
 from zilliz_gis.util.vega.heat_map.vega_heat_map import VegaHeatMap
+from zilliz_gis.util.vega.choropleth_map.choropleth_map import VegaChoroplethMap
 
 
 def test_vega_circle2d():
@@ -35,3 +36,20 @@ def test_vega_heat_map():
     assert vega_dict["width"] == 1900
     assert vega_dict["height"] == 1410
     assert vega_dict["marks"][0]["encode"]["enter"]["map_scale"]["value"] == 10.0
+
+def test_vega_choropleth_map():
+    vega_choropleth_map = VegaChoroplethMap(1900, 1410, [-73.984092,40.753893,-73.977588,40.756342], "blue_to_red", [2.5, 5], 1.0)
+    vega_json = vega_choropleth_map.build()
+    vega_dict = json.loads(vega_json)
+    assert vega_dict["width"] == 1900
+    assert vega_dict["height"] == 1410
+    assert len(vega_dict["marks"][0]["encode"]["enter"]["bounding_box"]["value"]) == 4
+    assert vega_dict["marks"][0]["encode"]["enter"]["bounding_box"]["value"][0] == -73.984092
+    assert vega_dict["marks"][0]["encode"]["enter"]["bounding_box"]["value"][1] == 40.753893
+    assert vega_dict["marks"][0]["encode"]["enter"]["bounding_box"]["value"][2] == -73.977588
+    assert vega_dict["marks"][0]["encode"]["enter"]["bounding_box"]["value"][3] == 40.756342
+    assert vega_dict["marks"][0]["encode"]["enter"]["color_style"]["value"] == "blue_to_red"
+    assert len(vega_dict["marks"][0]["encode"]["enter"]["ruler"]["value"]) == 2
+    assert vega_dict["marks"][0]["encode"]["enter"]["ruler"]["value"][0] == 2.5
+    assert vega_dict["marks"][0]["encode"]["enter"]["ruler"]["value"][1] == 5
+    assert vega_dict["marks"][0]["encode"]["enter"]["opacity"]["value"] == 1.0
