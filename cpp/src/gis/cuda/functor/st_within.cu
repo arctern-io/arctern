3b44f161f80571b2063b1c242ae4c5fece2ab1ea
fluorinedog <fluorinedog@gmail.com>
2020-02-22 21:05:22 +0800

    udpate

--- cpp/src/gis/cuda/functor/st_within.cu
+++ cpp/src/gis/cuda/functor/st_within.cu
@@ -29,11 +29,36 @@ namespace zilliz {
 namespace gis {
 namespace cuda {
 namespace {
+inline DEVICE_RUNNABLE bool PointInPolygonImpl(double pnt_x, double pnt_y,
+                                               const double* polygon,
+                                               int num_poly_vertexex) {
+  int winding_num = 0;
+  double dx2 = poly_xs[num_poly_vertexes - 1] - pnt_x;
+  double dy2 = poly_ys[num_poly_vertexes - 1] - pnt_y;
+  for (int poly_idx = 0; poly_idx < num_poly_vertexes; ++poly_idx) {
+    auto dx1 = dx2;
+    auto dy1 = dy2;
+    dx2 = poly_xs[poly_idx] - pnt_x;
+    dy2 = poly_ys[poly_idx] - pnt_y;
+    bool ref = dy1 < 0;
+    if (ref != (dy2 < 0)) {
+      if (isLeft(dx1, dy1, dx2, dy2) < 0 != ref) {
+        winding_num += ref ? 1 : -1;
+      }
+    }
+  }
+  uint8_t ans = winding_num != 0;
+  output[index] = ans;
+}
+
 inline DEVICE_RUNNABLE bool PointInPolygon(const GpuContext& point,
                                            const GpuContext& polygon, int index) {
   auto pv = point.get_value_ptr(index);
-  auto shape_size = polygon.get_meta_ptr(index);
-  //  for(auto shape_index = 0; )
+  auto polygon_meta = polygon.get_meta_ptr(index);
+  int shape_size = (int)polygon_meta[0];
+  for (int shape_index = 0; shape_index < shape_size; shape_index++) {
+    int poly_size = (int)polygon_meta[1 + shape_index];
+  }
   return false;
 }
 }  // namespace
