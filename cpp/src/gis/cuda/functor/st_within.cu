947669423fd5219be343243ca3c30be9c22ff74a
fluorinedog <fluorinedog@gmail.com>
2020-02-22 21:50:15 +0800

    point_in_polygon

--- cpp/src/gis/cuda/functor/st_within.cu
+++ cpp/src/gis/cuda/functor/st_within.cu
@@ -23,23 +23,35 @@
 #include <cmath>
 
 #include "gis/cuda/common/gpu_memory.h"
-#include "gis/cuda/functor/st_distance.h"
+#include "gis/cuda/functor/common.h"
+#include "gis/cuda/functor/st_within.h"
 
 namespace zilliz {
 namespace gis {
 namespace cuda {
 namespace {
+
+inline DEVICE_RUNNABLE double isLeft(double x1, double y1, double x2, double y2) {
+  return x1 * y2 - x2 * y1;
+}
+
+inline DEVICE_RUNNABLE double GetX(const double* polygon, int index) {
+  return polygon[2 * index];
+}
+inline DEVICE_RUNNABLE double GetY(const double* polygon, int index) {
+  return polygon[2 * index + 1];
+}
+
 inline DEVICE_RUNNABLE bool PointInPolygonImpl(double pnt_x, double pnt_y,
-                                               const double* polygon,
-                                               int num_poly_vertexex) {
+                                               const double* polygon, int size) {
   int winding_num = 0;
-  double dx2 = poly_xs[num_poly_vertexes - 1] - pnt_x;
-  double dy2 = poly_ys[num_poly_vertexes - 1] - pnt_y;
-  for (int poly_idx = 0; poly_idx < num_poly_vertexes; ++poly_idx) {
+  double dx2 = GetX(polygon, size - 1) - pnt_x;
+  double dy2 = GetY(polygon, size - 1) - pnt_y;
+  for (int index = 0; index < size; ++index) {
     auto dx1 = dx2;
     auto dy1 = dy2;
-    dx2 = poly_xs[poly_idx] - pnt_x;
-    dy2 = poly_ys[poly_idx] - pnt_y;
+    dx2 = GetX(polygon, index) - pnt_x;
+    dy2 = GetY(polygon, index) - pnt_y;
     bool ref = dy1 < 0;
     if (ref != (dy2 < 0)) {
       if (isLeft(dx1, dy1, dx2, dy2) < 0 != ref) {
@@ -47,38 +59,62 @@ inline DEVICE_RUNNABLE bool PointInPolygonImpl(double pnt_x, double pnt_y,
       }
     }
   }
-  uint8_t ans = winding_num != 0;
-  output[index] = ans;
+  return winding_num != 0;
 }
 
 inline DEVICE_RUNNABLE bool PointInPolygon(const GpuContext& point,
                                            const GpuContext& polygon, int index) {
   auto pv = point.get_value_ptr(index);
-  auto polygon_meta = polygon.get_meta_ptr(index);
-  int shape_size = (int)polygon_meta[0];
+  auto polygon_metas = polygon.get_meta_ptr(index);
+  auto polygon_values = polygon.get_value_ptr(index);
+  int shape_size = (int)polygon_metas[0];
+  // offsets of value for polygons
+  int offsets = 0;
+  bool final = false;
   for (int shape_index = 0; shape_index < shape_size; shape_index++) {
-    int poly_size = (int)polygon_meta[1 + shape_index];
+    int vertex_size = (int)polygon_metas[1 + shape_index];
+    auto is_in = PointInPolygonImpl(pv[0], pv[1], polygon_values + offsets, vertex_size);
+    if (shape_index == 0) {
+      final = is_in;
+    } else {
+      final = final && !is_in;
+    }
   }
-  return false;
+  return final;
+}
+
+__global__ void ST_WithinKernel(GpuContext left, GpuContext right, bool* result) {
+  auto tid = threadIdx.x + blockIdx.x * blockDim.x;
+  if (tid < left.size) {
+    auto left_tag = left.get_tag(tid);
+    auto right_tag = right.get_tag(tid);
+    // handle 2d case only for now
+    assert(left_tag.get_group() == WkbGroup::None);
+    assert(right_tag.get_group() == WkbGroup::None);
+    // handle point to point case only
+    if (left_tag.get_category() == WkbCategory::Point &&
+        right_tag.get_category() == WkbCategory::Polygon) {
+      result[tid] = PointInPolygon(left, right, tid);
+    } else {
+      result[tid] = false;
+    }
+  }
 }
 }  // namespace
 
-//__global__ void ST_WithinKernel(GpuContext left, GpuContext right, double* result) {}
-//}  // namespace
-//
-// void ST_Within(const GeometryVector& left_vec, const GeometryVector& right_vec,
-//               double* host_results) {
-//  assert(left_vec.size() == right_vec.size());
-//  auto left_ctx_holder = left_vec.CreateReadGpuContext();
-//  auto right_ctx_holder = right_vec.CreateReadGpuContext();
-//  auto dev_result = GpuMakeUniqueArray<double>(left_vec.size());
-//  {
-//    auto config = GetKernelExecConfig(left_vec.size());
-//    ST_WithinKernel<<<config.grid_dim, config.block_dim>>>(
-//        *left_ctx_holder, *right_ctx_holder, dev_result.get());
-//  }
-//  GpuMemcpy(host_results, dev_result.get(), left_vec.size());
-//}
+ void ST_Within(const GeometryVector& left_vec, const GeometryVector& right_vec,
+               bool* host_results) {
+  assert(left_vec.size() == right_vec.size());
+  auto left_ctx_holder = left_vec.CreateReadGpuContext();
+  auto right_ctx_holder = right_vec.CreateReadGpuContext();
+  auto dev_result = GpuMakeUniqueArray<bool>(left_vec.size());
+  {
+    auto config = GetKernelExecConfig(left_vec.size());
+    ST_WithinKernel<<<config.grid_dim, config.block_dim>>>(
+        *left_ctx_holder, *right_ctx_holder, dev_result.get());
+  }
+  GpuMemcpy(host_results, dev_result.get(), left_vec.size());
+}
 
 }  // namespace cuda
 }  // namespace gis
