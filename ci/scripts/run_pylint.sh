f175a512d941399a33c4ad4694773ab84d3e14a4
quicksilver <zhifeng.zhang@zilliz.com>
2020-02-24 17:07:00 +0800

    Open check cpplint & clang-format & pylint operation (#121)
    * Open check cpplint & clang-format & pylint operation
    
    * Open check cpplint & clang-format & pylint operation
    
    * Open check cpplint & clang-format & pylint operation
    
    * Open check cpplint & clang-format operation
    
    * update docker-compose.yml in deploy arctern spark

--- ci/scripts/run_pylint.sh
+++ ci/scripts/run_pylint.sh
@@ -2,7 +2,17 @@
 
 set -e
 
-cd "$(git rev-parse --show-toplevel)"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+REPO_ROOT_PATH="${SCRIPTS_DIR}/../.."
+
+cd "${REPO_ROOT_PATH}"
 
 # pylint3 was replaced with pylint from Ubuntu 19.10
 PYLINT=$(command -v pylint3) || true
@@ -18,4 +28,3 @@ find . -name \*.py \
 | sed 's/./\\&/g' \
 | xargs "${PYLINT}" -j 4 -ry --msg-template='{path}:{line}:{column}: {msg_id}: {msg} ({symbol})' --ignore="" \
 "$@"
-
