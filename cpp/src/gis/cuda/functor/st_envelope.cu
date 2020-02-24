2975e8847e048f6bea96afdfc8bd2bcaab583f17
fluorinedog <fluorinedog@gmail.com>
2020-02-24 20:51:05 +0800

    fix point bug

--- cpp/src/gis/cuda/functor/st_envelope.cu
+++ cpp/src/gis/cuda/functor/st_envelope.cu
@@ -60,7 +60,7 @@ DEVICE_RUNNABLE inline void AssertInfo(OutputInfo info, const GpuContext& ctx,
   assert(info.value_size == ctx.value_offsets[index + 1] - ctx.value_offsets[index]);
 }
 
-static __global__ void FillDataKernel(const double* xs, const double* ys,
+static __global__ void FillDataKernel(const GpuContext input,
                                       GpuContext results) {
   assert(results.data_state == DataState::PrefixSumOffset_EmptyData);
   auto index = threadIdx.x + blockIdx.x * blockDim.x;
@@ -71,41 +71,41 @@ static __global__ void FillDataKernel(const double* xs, const double* ys,
 }
 }  // namespace
 
-void ST_Point(const double* cpu_xs, const double* cpu_ys, int size,
-              GeometryVector& results) {
+void ST_Envelope(const GeometryVector& input_vec,
+                 GeometryVector& results){
   // copy xs, ys to gpu
-  auto xs = GpuMakeUniqueArrayAndCopy(cpu_xs, size);
-  auto ys = GpuMakeUniqueArrayAndCopy(cpu_ys, size);
+  auto input_holder = input_vec.CreateReadGpuContext();
+  auto size = input_vec.size();
 
   // STEP 1: Initialize vector with size of elements
   results.OutputInitialize(size);
 
   // STEP 2: Create gpu context according to the vector for cuda
   // where tags and offsets fields are uninitailized
-  auto ctx_holder = results.OutputCreateGpuContext();
+  auto results_holder = results.OutputCreateGpuContext();
   {
     // STEP 3: Fill info(tags and offsets) to gpu_ctx using CUDA Kernels
     // where offsets[0, n) is filled with size of each element
     auto config = GetKernelExecConfig(size);
-    FillInfoKernel<<<config.grid_dim, config.block_dim>>>(xs.get(), ys.get(),
-                                                          *ctx_holder);
-    ctx_holder->data_state = DataState::FlatOffset_FullInfo;
+    FillInfoKernel<<<config.grid_dim, config.block_dim>>>(*input_holder,
+                                                          *results_holder);
+    results_holder->data_state = DataState::FlatOffset_FullInfo;
   }
 
   // STEP 4: Exclusive scan offsets[0, n+1), where offsets[n] = 0
   // then copy info(tags and scanned offsets) back to GeometryVector
   // and alloc cpu & gpu memory for next steps
-  results.OutputEvolveWith(*ctx_holder);
+  results.OutputEvolveWith(*results_holder);
 
   {
     // STEP 5: Fill data(metas and values) to gpu_ctx using CUDA Kernels
     auto config = GetKernelExecConfig(size);
-    FillDataKernel<<<config.grid_dim, config.block_dim>>>(xs.get(), ys.get(),
-                                                          *ctx_holder);
-    ctx_holder->data_state = DataState::PrefixSumOffset_FullData;
+    FillDataKernel<<<config.grid_dim, config.block_dim>>>(*input_holder,
+                                                          *results_holder);
+    results_holder->data_state = DataState::PrefixSumOffset_FullData;
   }
   // STEP 6: Copy data(metas and values) back to GeometryVector
-  results.OutputFinalizeWith(*ctx_holder);
+  results.OutputFinalizeWith(*results_holder);
 }
 
 }  // namespace cuda
