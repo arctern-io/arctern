1f2380a42df36d856c42a6327a65f50191765a9e
quicksilver <zhifeng.zhang@zilliz.com>
2020-02-27 22:31:18 +0800

    update conda build scripts file

--- conda/recipes/arctern/cpu/build.sh
+++ conda/recipes/arctern/cpu/build.sh
@@ -4,6 +4,6 @@ printenv
 # Cleanup local git
 git clean -xdf
 ./ci/scripts/cpp_build.sh clean
-./ci/scripts/cpp_build.sh --build_type=Release -l -v
+./ci/scripts/cpp_build.sh --build_type=Release -v
 ./ci/scripts/python_build.sh clean
 ./ci/scripts/python_build.sh
--- conda/recipes/arctern/gpu/build.sh
+++ conda/recipes/arctern/gpu/build.sh
@@ -5,6 +5,6 @@ printenv
 git clean -xdf
 
 ./ci/scripts/cpp_build.sh clean
-./ci/scripts/cpp_build.sh --build_type=Release -l -g -v
+./ci/scripts/cpp_build.sh --build_type=Release -g -v
 ./ci/scripts/python_build.sh clean
 ./ci/scripts/python_build.sh
