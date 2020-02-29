29b7bb4b0e754be0145a30f8f2026e4af2b2f420
quicksilver <zhifeng.zhang@zilliz.com>
2020-02-29 12:11:00 +0800

    update conda integration script file

--- conda/recipes/arctern-spark/build.sh
+++ conda/recipes/arctern-spark/build.sh
@@ -1,8 +1,5 @@
 # show environment
 printenv
 
-# Cleanup local git
-git clean -xdf
-
 ./ci/scripts/pyspark_build.sh clean
 ./ci/scripts/pyspark_build.sh
--- conda/recipes/arctern/build.sh
+++ conda/recipes/arctern/build.sh
@@ -1,8 +1,5 @@
 # show environment
 printenv
 
-# Cleanup local git
-git clean -xdf
-
 ./ci/scripts/python_build.sh clean
 ./ci/scripts/python_build.sh
