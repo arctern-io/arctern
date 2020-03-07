07dfa4452ba3e06150c86875139f093a095d6403
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-07 17:48:32 +0800

    Update upload package file script

--- ci/scripts/conda/cpu/upload_package.sh
+++ ci/scripts/conda/cpu/upload_package.sh
@@ -7,29 +7,10 @@ if [ -z "$UPLOAD_PACKAGE_FILE_KEY" ]; then
     return 0
 fi
 
-if [ "$UPLOAD_LIBARCTERN" == "1" ]; then
-    export LIBARCTERN_FILE=`conda build conda/recipes/libarctern/cpu -c conda-forge -c defaults --output`
-
-    test -e ${LIBARCTERN_FILE}
-    echo "Upload libarctern package file ..."
-    echo ${LIBARCTERN_FILE}
-    curl -u${JFROG_USENAME:-arctern}:${UPLOAD_PACKAGE_FILE_KEY} -T ${LIBARCTERN_FILE} ${ARTFACTORY_URL}/${LIBARCTERN_FILE##*/}
-fi
-
-if [ "$UPLOAD_ARCTERN" == "1" ]; then
-    export ARCTERN_FILE=`conda build conda/recipes/arctern -c conda-forge -c defaults --output`
-
-    test -e ${ARCTERN_FILE}
-    echo "Upload arctern package file ..."
-    echo ${ARCTERN_FILE}
-    curl -u${JFROG_USENAME:-arctern}:${UPLOAD_PACKAGE_FILE_KEY} -T ${ARCTERN_FILE} ${ARTFACTORY_URL}/${ARCTERN_FILE##*/}
-fi
-
-if [ "$UPLOAD_ARCTERN_SPARK" == "1" ]; then
-    export ARCTERN_SPARK_FILE=`conda build conda/recipes/arctern-spark -c conda-forge -c defaults --output`
-
-    test -e ${ARCTERN_SPARK_FILE}
-    echo "Upload arctern-spark package file..."
-    echo ${ARCTERN_SPARK_FILE}
-    curl -u${JFROG_USENAME:-arctern}:${UPLOAD_PACKAGE_FILE_KEY} -T ${ARCTERN_SPARK_FILE} ${ARTFACTORY_URL}/${ARCTERN_SPARK_FILE##*/}
+if [ "$UPLOAD_LIBARCTERN" == "1" || "$UPLOAD_ARCTERN" == "1" || "$UPLOAD_ARCTERN_SPARK" == "1" ]; then
+    if [ -d ${CONDA_PREFIX}/conda-bld ];then
+        tar -zcvf ./conda-bld.tar.gz -C ${CONDA_PREFIX}/ conda-bld
+        curl -u${JFROG_USENAME:-arctern}:${UPLOAD_PACKAGE_FILE_KEY} -T ./conda-bld.tar.gz ${ARTFACTORY_URL}/conda-bld.tar.gz
+        rm ./conda-bld.tar.gz
+    fi
 fi
