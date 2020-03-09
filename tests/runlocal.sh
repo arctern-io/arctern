e59c04a8ea7fe62c1747803212be2be03528333e
liang.liu <liang.liu@zilliz.com>
2020-03-09 16:51:18 +0800

    remove runlocal script

--- tests/runlocal.sh
+++
@@ -1,5 +0,0 @@
-/usr/local/bin/spark/bin/spark-submit /home/liangliu/workspace/arctern/tests/spark_test.py
-# /opt/spark/bin/spark-submit /home/arctern_test/tests/spark_test.py
-
-python collect_results.py
-python compare.py
