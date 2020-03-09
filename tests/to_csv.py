f43b526642433f9930fc381aa12f5a35693fbc78
liang.liu <liang.liu@zilliz.com>
2020-03-09 17:37:17 +0800

    update path of to_csv function

--- tests/to_csv.py
+++ tests/to_csv.py
@@ -1,24 +1,22 @@
 import os
 from pyspark.sql import SparkSession
+import sys
 
-json_dir = '/home/liangliu/workspace/arctern/tests/data'
-data_dir = '/home/liangliu/workspace/arctern/tests/data/csv'
+json_dir = sys.argv[1]
+data_dir = sys.argv[2]
 
 def json2csv(spark):
     f_list = os.listdir(json_dir)
     for i in f_list:
         fs = os.path.splitext(i)
         if fs[1] == '.json':
-            #print(i)
-            # df = spark.read.json("/home/czp/tele/tests/data/%s" % i).cache()
-            # df.write.csv("/home/czp/tele/tests/data/csv/%s.csv" % fs[0],sep='|')
             df = spark.read.json(os.path.join(json_dir, i)).cache()
             df.write.csv(os.path.join(data_dir, fs[0] + '.csv'), sep='|')
 
 
 
 if __name__ == '__main__':
-    # path = data_dir
+    
     spark_session = SparkSession \
             .builder \
             .appName("json to csv script.") \
@@ -27,4 +25,5 @@ if __name__ == '__main__':
     spark_session.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
     json2csv(spark_session)
     spark_session.stop()
+    
 
