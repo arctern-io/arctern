4199b019590955a2d9594908fb5265c6c8e3afaa
liang.liu <liang.liu@zilliz.com>
2020-03-09 10:21:37 +0800

    update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,5 @@
-/usr/local/bin/spark/bin/spark-submit /home/liangliu/workspace/arctern/tests/spark_test.py
+# /usr/local/bin/spark/bin/spark-submit /home/liangliu/workspace/arctern/tests/spark_test.py
+/opt/spark/bin/spark-submit /home/arctern_test/tests/spark_test.py
 
-# python collect_results.py
-# python compare.py
+python collect_results.py
+python compare.py
