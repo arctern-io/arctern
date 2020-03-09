7cffeda80faeff72c6162f591eb6fa3b07dd4874
fluorinedog <fluorinedog@gmail.com>
2020-03-09 10:21:14 +0800

    format

--- cpp/src/gis/cuda/conversion/wkb_visitor.h
+++ cpp/src/gis/cuda/conversion/wkb_visitor.h
@@ -1,10 +1,26 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 #pragma once
 #include "gis/cuda/wkb/wkb_tag.h"
 
 namespace arctern {
 namespace gis {
 namespace cuda {
-
 
 template <typename WkbVisitorImpl>
 struct WkbCodingVisitor : public WkbVisitorImpl {
@@ -24,8 +40,8 @@ struct WkbCodingVisitor : public WkbVisitorImpl {
   __device__ void VisitPolygon(int dimensions) {
     auto size = VisitMetaInt();
     for (int i = 0; i < size; ++i) {
-//      auto polys = VisitMetaInt();
-//      VisitValues(dimensions, polys);
+      //      auto polys = VisitMetaInt();
+      //      VisitValues(dimensions, polys);
       VisitLineString(dimensions);
     }
   }
@@ -36,7 +52,6 @@ struct WkbCodingVisitor : public WkbVisitorImpl {
     assert(expected_dimensions == 2);
     assert(tag.get_space_type() == WkbSpaceType::XY);
   }
-
 
   __device__ void VisitMultiPoint(int dimensions) {
     auto size = VisitMetaInt();
