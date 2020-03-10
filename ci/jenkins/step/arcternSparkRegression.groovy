4499508f99a5ac1def437a21f8dee5555d8375d2
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-10 23:36:53 +0800

    Update arcternSparkRegression.groovy

--- ci/jenkins/step/arcternSparkRegression.groovy
+++ ci/jenkins/step/arcternSparkRegression.groovy
@@ -3,12 +3,14 @@ timeout(time: 10, unit: 'MINUTES') {
         dir ("docker/test_env/spark/${BINARY_VERSION}") {
             sh "docker-compose -p ${env.PIPELINE_NAME}-${env.BUILD_NUMBER} --compatibility up -d"
         }
-        dir ("docker/test_env/spark/${BINARY_VERSION}") { 
+        dir ("docker/test_env") {
             sh "docker-compose -p ${env.PIPELINE_NAME}-${env.BUILD_NUMBER} --compatibility run --rm regression"
         }
     } catch(exc) {
         throw exc
     } finally {
-        sh "docker-compose -p ${env.PIPELINE_NAME}-${env.BUILD_NUMBER} --compatibility down"
+        dir ("docker/test_env/spark/${BINARY_VERSION}") {
+            sh "docker-compose -p ${env.PIPELINE_NAME}-${env.BUILD_NUMBER} --compatibility down"
+        }
     }
 }
