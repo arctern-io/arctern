f8016b029a17d4efb1d3bbd3ce661b3d193c042f
liang.liu <liang.liu@zilliz.com>
2020-03-10 10:58:42 +0800

    add st_crosses postgis sql

--- tests/expected/sqls/st_crosses.sql
+++ tests/expected/sqls/st_crosses.sql
@@ -1,7 +1,7 @@
 drop table if exists t1;
 create table t1 (geo1 geometry, geo2 geometry);
-copy t1 from '@path@/crosses.csv' DELIMITER '|' csv header;
+copy t1 from '@path@/data/crosses.csv' DELIMITER '|' csv header;
 
-\o @path@/st_crosses.out
+\o @path@/expected/results/st_crosses.out
 select st_crosses(geo1, geo2) from t1;
 \o
