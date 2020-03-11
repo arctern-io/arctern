7c3cbfb80211a1dc60483749fa3c75355e12f87a
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-11 10:05:09 +0800

    Update arctern spark test environement docker compose file

--- ci/jenkins/step/arcternSparkRegression.groovy
+++ ci/jenkins/step/arcternSparkRegression.groovy
@@ -1,16 +1,22 @@
 timeout(time: 10, unit: 'MINUTES') {
+    def composeProject = "${env.PIPELINE_NAME}-${env.BUILD_NUMBER}"
+
+    if ("${BINARY_VERSION}" == "gpu") {
+        composeProject = "${env.PIPELINE_NAME}-${BINARY_VERSION}-${env.BUILD_NUMBER}"
+    }
+
     try {
         dir ("docker/test_env/spark/${BINARY_VERSION}") {
-            sh "docker-compose -p ${env.PIPELINE_NAME}-${env.BUILD_NUMBER} --compatibility up -d"
+            sh "docker-compose -p ${composeProject} --compatibility up -d"
         }
         dir ("docker/test_env") {
-            sh "docker-compose -p ${env.PIPELINE_NAME}-${env.BUILD_NUMBER} --compatibility run --rm regression"
+            sh "docker-compose -p ${composeProject} --compatibility run --rm regression"
         }
     } catch(exc) {
         throw exc
     } finally {
         dir ("docker/test_env/spark/${BINARY_VERSION}") {
-            sh "docker-compose -p ${env.PIPELINE_NAME}-${env.BUILD_NUMBER} --compatibility down"
+            sh "docker-compose -p ${composeProject} --compatibility down"
         }
     }
 }
