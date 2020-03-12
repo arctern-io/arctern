696e35d947349432b51aad60543a4a2a95423490
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-12 20:58:35 +0800

    Add Code coverage in Jenkins CI

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -1,9 +1,9 @@
 timeout(time: 15, unit: 'MINUTES') {
     dir ("ci/scripts") {
         if ("${BINARY_VERSION}" == "gpu") {
-            sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"zgis_dev\" -l -g -u'"
+            sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"zgis_dev\" -l -g -u --coverage'"
         } else {
-            sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"zgis_dev\" -l -u'"
+            sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"zgis_dev\" -l -u --coverage'"
         }
         sh "/bin/bash --login -c './python_build.sh -e \"zgis_dev\"'"
     }
