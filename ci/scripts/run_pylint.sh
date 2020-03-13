c0fc2d36dc11fbe183c079d253dc4b2c86562ae2
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-13 21:01:30 +0800

    Add python build workflow in github action

--- ci/scripts/run_pylint.sh
+++ ci/scripts/run_pylint.sh
@@ -14,17 +14,11 @@ REPO_ROOT_PATH="${SCRIPTS_DIR}/../.."
 
 cd "${REPO_ROOT_PATH}"
 
-# pylint3 was replaced with pylint from Ubuntu 19.10
-PYLINT=$(command -v pylint3) || true
-if [ -z "$PYLINT" ]; then
-    PYLINT=$(command -v pylint)
-fi
-
 find . -name \*.py \
 	-and -not -path ./cpp/\* \
 	-and -not -path ./ci/\* \
 	-and -not -path ./doc/\* \
 	-and -not -path ./docker/\* \
 | sed 's/./\\&/g' \
-| xargs "${PYLINT}" -j 4 -ry --msg-template='{path}:{line}:{column}: {msg_id}: {msg} ({symbol})' --ignore="" \
-"$@"
+| xargs pylint -j 4 -ry --msg-template='{path}:{line}:{column}: {msg_id}: {msg} ({symbol})' --ignore="" \
+"$@" || exit 1
