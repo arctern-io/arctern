55d56c9f83b0c81397017476fded214232182200
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-13 21:09:24 +0800

    Add python build workflow in github action

--- ci/scripts/run_pylint.sh
+++ ci/scripts/run_pylint.sh
@@ -12,6 +12,40 @@ SCRIPTS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 REPO_ROOT_PATH="${SCRIPTS_DIR}/../.."
 
+HELP="
+Usage:
+  $0 [flags] [Arguments]
+
+    -e [CONDA_ENV] or --conda_env=[CONDA_ENV]
+                              Setting conda activate environment
+    -h or --help              Print help information
+
+
+Use \"$0  --help\" for more information about a given command.
+"
+
+ARGS=`getopt -o "e:h" -l "conda_env::,help" -n "$0" -- "$@"`
+
+eval set -- "${ARGS}"
+
+while true ; do
+        case "$1" in
+                -e|--conda_env)
+                        case "$2" in
+                                "") echo "Option conda_env, no argument"; exit 1 ;;
+                                *)  CONDA_ENV=$2 ; shift 2 ;;
+                        esac ;;
+                -h|--help) echo -e "${HELP}" ; exit 0 ;;
+                --) shift ; break ;;
+                *) echo "Internal error!" ; exit 1 ;;
+        esac
+done
+
+if [[ -n ${CONDA_ENV} ]]; then
+    eval "$(conda shell.bash hook)"
+    conda activate ${CONDA_ENV}
+fi
+
 cd "${REPO_ROOT_PATH}"
 
 find . -name \*.py \
