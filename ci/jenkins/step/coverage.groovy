debe0d26193319314ee9faee92c5e78bfb81bcff
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-16 10:25:14 +0800

    Add codecov upload in Github Action

--- ci/jenkins/step/coverage.groovy
+++ ci/jenkins/step/coverage.groovy
@@ -1,7 +1,7 @@
 timeout(time: 5, unit: 'MINUTES') {
     dir ("ci/scripts") {
     	withCredentials([[$class: 'StringBinding', credentialsId: "arctern-ci-codecov-token", variable: 'CODECOV_TOKEN']]) {
-            sh "/bin/bash --login -c './coverage.sh -e \"zgis_dev\"'"
+            sh "/bin/bash --login -c './coverage.sh -e \"zgis_dev\" -n ${CPU_ARCH}-${OS_NAME}-${BINARY_VERSION}-unittest -F ${CPU_ARCH}-${OS_NAME}-${BINARY_VERSION}-unittest '"
         }
     }
 }
