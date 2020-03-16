ec68f946b1fb6ac90035909d3228674ad32e1f1e
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-16 12:00:30 +0800

    Fix codecov upload bug

--- ci/jenkins/step/coverage.groovy
+++ ci/jenkins/step/coverage.groovy
@@ -1,7 +1,7 @@
 timeout(time: 5, unit: 'MINUTES') {
     dir ("ci/scripts") {
     	withCredentials([[$class: 'StringBinding', credentialsId: "arctern-ci-codecov-token", variable: 'CODECOV_TOKEN']]) {
-            sh "/bin/bash --login -c './coverage.sh -e \"zgis_dev\" -n ${CPU_ARCH}-linux-${BINARY_VERSION}-unittest -F ${CPU_ARCH}-linux-${BINARY_VERSION}-unittest '"
+            sh "/bin/bash --login -c './coverage.sh -e \"zgis_dev\" -n ${CPU_ARCH}-linux-${BINARY_VERSION}-unittest -F ${CPU_ARCH}_linux_${BINARY_VERSION}_unittest '"
         }
     }
 }
