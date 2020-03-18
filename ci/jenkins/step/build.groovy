59307d8fb29dbb1e51499dcb1407de3cd9961e89
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-18 16:24:34 +0800

    Add ccache in Jenkins CI

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -1,16 +1,16 @@
 timeout(time: 15, unit: 'MINUTES') {
-    def checkResult = sh(script: "./check_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache", returnStatus: true)
-
     dir ("ci/scripts") {
+        def checkResult = sh(script: "./check_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache", returnStatus: true)
+
         if ("${BINARY_VERSION}" == "gpu") {
             sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"zgis_dev\" -l -g -u --coverage'"
         } else {
             sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"zgis_dev\" -l -u --coverage'"
         }
         sh "/bin/bash --login -c './python_build.sh -e \"zgis_dev\"'"
-    }
 
-    withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        def updateResult = sh(script: "./update_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache -u ${USERNAME} -p ${PASSWORD}", returnStatus: true)
+        withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+            def updateResult = sh(script: "./update_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache -u ${USERNAME} -p ${PASSWORD}", returnStatus: true)
+        }
     }
 }
