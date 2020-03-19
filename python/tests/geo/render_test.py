faea3f1f4fc8dd77a4bb9cf8be07d8e5d4e390bd
xi.ge <xi.ge@zilliz.com>
2020-03-19 16:12:42 +0800

    fix python vega test

--- python/tests/geo/render_test.py
+++ python/tests/geo/render_test.py
@@ -40,7 +40,7 @@ def test_point_map():
     arr_x = pandas.Series(x_data)
     arr_y = pandas.Series(y_data)
 
-    vega_circle2d = vega_pointmap(1024, 896, 'POINT (-73.998427 40.780816)', 'POINT (-73.954348 40.730309)', 3, "#2DEF4A", 0.5, "EPSG:43")
+    vega_circle2d = vega_pointmap(1024, 896, [-73.998427, 40.730309, -73.954348, 40.780816], 3, "#2DEF4A", 0.5, "EPSG:43")
     vega_json = vega_circle2d.build()
 
     curve_z = arctern.point_map(arr_x, arr_y, vega_json.encode('utf-8'))
@@ -60,7 +60,7 @@ def test_heat_map():
     arr_y = pandas.Series(y_data)
     arr_c = pandas.Series(y_data)
 
-    vega_heat_map = vega_heatmap(1024, 896, 10.0, 'POINT (-73.998427 40.780816)', 'POINT (-73.954348 40.730309)', 'EPSG:4326')
+    vega_heat_map = vega_heatmap(1024, 896, 10.0, [-73.998427, 40.730309, -73.954348, 40.780816], 'EPSG:4326')
     vega_json = vega_heat_map.build()
 
     heat_map = arctern.heat_map(arr_x, arr_y, arr_c, vega_json.encode('utf-8'))
@@ -81,7 +81,7 @@ def test_choropleth_map():
     arr_wkt = pandas.Series(wkt_data)
     arr_count = pandas.Series(count_data)
 
-    vega_choropleth_map = vega_choroplethmap(1900, 1410, 'POINT (-73.994092 40.759642)', 'POINT (-73.977588 40.753893)', "blue_to_red", [2.5, 5], 1.0, 'EPSG:4326')
+    vega_choropleth_map = vega_choroplethmap(1900, 1410, [-73.994092, 40.753893, -73.977588, 40.759642], "blue_to_red", [2.5, 5], 1.0, 'EPSG:4326')
     vega_json = vega_choropleth_map.build()
 
     choropleth_map = arctern.choropleth_map(arr_wkt, arr_count, vega_json.encode('utf-8'))
