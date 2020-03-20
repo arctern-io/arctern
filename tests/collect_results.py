c0ec578c26282c39fe2be9241b4bc5da547d29d9
y.d.s.l <exceptionliu@gmail.com>
2020-03-20 16:12:14 +0800

    fuck pylint

--- tests/collect_results.py
+++ tests/collect_results.py
@@ -9,18 +9,19 @@ from util import get_tests
 
 
 def collect_results():
+    """Collect spark results from different dirs."""
     if not os.path.isdir(arctern_result):
         os.makedirs(arctern_result)
 
     names, table_names, expects = get_tests()
 
     base_dir = spark_result
-    for x, y in zip(table_names, names):
-        # print(x, y)
-        target = os.path.join(base_dir, x, '*.csv')
+    for table_name, name in zip(table_names, names):
+        target = os.path.join(base_dir, table_name, '*.csv')
         file_name = glob.glob(target)
         if os.path.isfile(file_name[0]):
-            shutil.copyfile(file_name[0], os.path.join(arctern_result, y + '.csv'))
+            shutil.copyfile(file_name[0], os.path.join(
+                arctern_result, name + '.csv'))
         else:
             print('file [%s] not exist' % file_name[0])
 
