ec8d8cf84d4086406a449236df02fd5edb1c4885
y.d.s.l <exceptionliu@gmail.com>
2020-03-20 17:20:13 +0800

    fuck pylint

--- tests/compare.py
+++ tests/compare.py
@@ -1,8 +1,22 @@
+# Copyright (C) 2019-2020 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # import glob
 from shapely import wkt
 from ogr import Geometry
 from ogr import CreateGeometryFromWkt
-from util import *
+# from util import *
 
 
 GEO_TYPES = ['POLYGON', 'POINT', 'LINESTRING']
@@ -30,8 +44,9 @@ def is_length_types(geo):
     """Determine whether a geometry is point/linestring/multipoint/multilinestring."""
     geo = geo.strip().upper()
 
-    for x in GEO_LENGTH_TYPES:
-        if geo.startswith(x) and len(geo) != len(x):
+    for a_geo_type_in_geo_length_types_list in GEO_LENGTH_TYPES:
+        if geo.startswith(a_geo_type_in_geo_length_types_list) and \
+                len(geo) != len(a_geo_type_in_geo_length_types_list):
             return True
 
         continue
@@ -43,11 +58,12 @@ def is_area_types(geo):
     """Determine whether a geometry is polygon/multipolygon."""
     geo = geo.strip().upper()
 
-    for x in GEO_AREA_TYPES:
-        if geo.startswith(x) and len(geo) != len(x):
+    for a_geo_type_in_geo_area_types_list in GEO_AREA_TYPES:
+        if geo.startswith(a_geo_type_in_geo_area_types_list) and \
+                len(geo) != len(a_geo_type_in_geo_area_types_list):
             return True
-        else:
-            continue
+        # else:
+        continue
 
     return False
 
@@ -62,11 +78,11 @@ def is_geometrytype(geo):
     arr.extend(CURVE_TYPES)
     arr.extend(SURFACE_TYPES)
 
-    for x in arr:
-        if x in geo:
+    for a_geo_type_in_all_geo_types_list in arr:
+        if a_geo_type_in_all_geo_types_list in geo:
             return True
-        else:
-            continue
+        # else:
+        continue
 
     return False
 
@@ -75,11 +91,11 @@ def is_curve(geo):
     """Determine whether a geometry is curve types, like circularstring/MULTICURVE/COMPOUNDCURVE."""
     geo = geo.strip().upper()
 
-    for x in CURVE_TYPES:
-        if geo.startswith(x):
+    for a_geo_type_in_curve_geo_types_list in CURVE_TYPES:
+        if geo.startswith(a_geo_type_in_curve_geo_types_list):
             return True
-        else:
-            continue
+        # else:
+        continue
 
     return False
 
@@ -88,11 +104,11 @@ def is_surface(geo):
     """Determine whether a geometry is curve types, like CURVEPOLYGON/MULTISURFACE/SURFACE."""
     geo = geo.strip().upper()
 
-    for x in SURFACE_TYPES:
-        if geo.startswith(x):
+    for a_geo_type_in_surface_geo_types_list in SURFACE_TYPES:
+        if geo.startswith(a_geo_type_in_surface_geo_types_list):
             return True
-        else:
-            continue
+        # else:
+        continue
 
     return False
 
@@ -161,12 +177,15 @@ def compare_geometry(c, x, y):
 def compare_geometrycollection(c, x, y):
     """Compare whether 2 geometrycollections is 'equal'."""
     if c in BLIST:
-        if arc_distance(x, y) < EPOCH_CURVE_RELATIVE:
-            return True
-        else:
-            print('arc distance: %s' % str(arc_distance(x, y)))
-            return False
-    else:
+        # print('arc distance: %s' % str(arc_distance(x, y)))
+        return arc_distance(x, y) < EPOCH_CURVE_RELATIVE
+        #     return True
+        # else:
+        #     print('arc distance: %s' % str(arc_distance(x, y)))
+        #     return False
+    # else:
+
+    if not c in BLIST:
         arct = wkt.loads(x)
         pgis = wkt.loads(y)
         result = arct.equals(pgis)
@@ -175,50 +194,35 @@ def compare_geometrycollection(c, x, y):
 
 def compare_floats(c, x, y):
     """Compare whether 2 float values is 'equal'."""
-    x = float(x)
-    y = float(y)
-    if x == 0:
-        if y == 0:
-            return True
-        else:
-            return False
+    value_x = float(x)
+    value_y = float(y)
+    if value_x == 0:
+        return value_y == 0
+
     if c in ALIST:
         precision_error = EPOCH_CURVE_RELATIVE
-        return abs((x - y)) <= precision_error
-        # return compare2float_relative(x, y, precision_error)
     else:
         precision_error = EPOCH
-    if abs((x - y)) <= precision_error:
-        return True
-    else:
-        # print(x, y)
-        return False
+
+    return abs((value_x - value_y)) <= precision_error
 
 
 def compare_float(x, y, z, precision_error):
     """Compare whether 2 geometries and their intersection is 'equal'."""
 
-    x = float(x)
-    y = float(y)
-    z = float(z)
-    if abs((x - y)) <= precision_error and abs(
-            (x - z)) <= precision_error and abs((y - z)) <= precision_error:
-        return True
-    else:
-        # print(x, y)
-        return False
+    value_x = float(x)
+    value_y = float(y)
+    value_z = float(z)
+    return abs((value_x - value_y)) <= precision_error and \
+        abs((value_x - value_z)) <= precision_error and \
+        abs((value_y - value_z)) <= precision_error
 
 
 def compare2float_relative(x_base, y_check, relative_error):
     """Compare whether 2 geometries and their intersection is 'equal', measure with relative."""
-    x = float(x_base)
-    y = float(y_check)
-    if ((abs(x_base - y_check)) / (abs(x_base))) <= relative_error:
-        return True
-    else:
-        print('arctern: %s, postgis: %s, precision_error: %s' %
-              (str(x), str(y), str(relative_error)))
-        return False
+    value_x = float(x_base)
+    value_y = float(y_check)
+    return ((abs(value_x - value_y)) / (abs(value_x))) <= relative_error
 
 
 def compare3float_relative(x_base, y_check, z_intersection, relative_error):
@@ -268,78 +272,80 @@ def convert_str(strr):
         x = float(strr)
         return x
     except ValueError as e:
-        # print(repr(e))
-        pass
+        print(repr(e))
+        # pass
 
     return strr
 
 
 def compare_one(config, result, expect):
     """Compare 1 line of arctern result and expected."""
-    x = result[1]
-    y = expect[1]
+    value_x = result[1]
+    value_y = expect[1]
     # c = config
 
-    x = convert_str(x)
-    y = convert_str(y)
+    newvalue_x = convert_str(value_x)
+    newvalue_y = convert_str(value_y)
 
     try:
-        if isinstance(x, bool):
-            flag = (x == y)
+        if isinstance(newvalue_x, bool):
+            flag = (newvalue_x == newvalue_y)
             if not flag:
-                print(result[0], x, expect[0], y)
+                print(result[0], newvalue_x, expect[0], newvalue_y)
             return flag
 
-        if isinstance(x, str):
-            x = x.strip().upper()
-            y = y.strip().upper()
+        if isinstance(newvalue_x, str):
+            newvalue_x = newvalue_x.strip().upper()
+            newvalue_y = newvalue_y.strip().upper()
 
             # check order : empty -> GEO_TYPES -> geocollection_types -> curve -> surface
-            if (is_empty(x) and is_empty(y)):
+            if (is_empty(newvalue_x) and is_empty(newvalue_y)):
                 return True
 
-            elif is_geometry(x) and is_geometry(y):
-                flag = compare_geometry(config, x, y)
+            elif is_geometry(newvalue_x) and is_geometry(newvalue_y):
+                flag = compare_geometry(config, newvalue_x, newvalue_y)
                 if not flag:
-                    print(result[0], x, expect[0], y)
+                    print(result[0], newvalue_x, expect[0], newvalue_y)
                 return flag
 
-            elif is_geometrycollection(x) and is_geometrycollection(y):
-                flag = compare_geometrycollection(config, x, y)
+            elif is_geometrycollection(newvalue_x) and is_geometrycollection(newvalue_y):
+                flag = compare_geometrycollection(
+                    config, newvalue_x, newvalue_y)
                 if not flag:
-                    print(result[0], x, expect[0], y)
+                    print(result[0], newvalue_x, expect[0], newvalue_y)
                 return flag
             else:
-                if is_geometrytype(x) and is_geometrytype(y):
-                    flag = (x == y)
+                if is_geometrytype(newvalue_x) and is_geometrytype(newvalue_y):
+                    flag = (newvalue_x == newvalue_y)
                     if not flag:
-                        print(result[0], x, expect[0], y)
+                        print(result[0], newvalue_x, expect[0], newvalue_y)
                     return flag
 
-                print(result[0], x, expect[0], y)
+                print(result[0], newvalue_x, expect[0], newvalue_y)
                 return False
 
-        if isinstance(x, int) or isinstance(x, float):
-            flag = compare_floats(config, x, y)
+        if isinstance(newvalue_x, int) or isinstance(newvalue_x, float):
+            flag = compare_floats(config, newvalue_x, newvalue_y)
             if not flag:
-                print(result[0], x, expect[0], y)
+                print(result[0], newvalue_x, expect[0], newvalue_y)
             return flag
-    except Exception as e:
+    except ValueError as e:
+		print(repr(e))
         flag = False
     return flag
 
 
 def compare_results(config, arctern_results, postgis_results):
     """Compare the result of arctern function and expected."""
-    with open(arctern_results, 'r') as f:
+    with open(arctern_results, 'r') as arctern_result_file:
         arct_arr = []
-        for (num, value) in enumerate(f, 1):
+        for (num, value) in enumerate(arctern_result_file, 1):
             if value.strip() != '':
                 arct_arr.append((num, value.strip()))
 
-    with open(postgis_results, 'r') as f:
+    with open(postgis_results, 'r') as postgis_result_file:
         pgis_arr = []
-        for (num, value) in enumerate(f, 1):
+        for (num, value) in enumerate(postgis_result_file, 1):
             if value.strip() != '':
                 pgis_arr.append((num, value.strip()))
 
@@ -351,8 +357,8 @@ def compare_results(config, arctern_results, postgis_results):
             % (len(arct_arr), len(pgis_arr)))
         return False
 
-    for x, y in zip(arct_arr, pgis_arr):
-        res = compare_one(config, x, y)
+    for a_line_in_arctern_result_file, a_line_in_postgis_result_file in zip(arct_arr, pgis_arr):
+        res = compare_one(config, a_line_in_arctern_result_file, a_line_in_postgis_result_file)
         flag = flag and res
 
     return flag
@@ -406,7 +412,8 @@ def update_bool(file_path):
         content = the_result_file_from_spark_for_read_and_abbr_not_allowed_by_pylint.read()
         update = content.replace('true', 'True').replace('false', 'False')
     with open(file_path, 'w') as the_result_file_from_spark_for_write_and_abbr_not_allowed_by_pylint:
-        the_result_file_from_spark_for_write_and_abbr_not_allowed_by_pylint.write(update)
+        the_result_file_from_spark_for_write_and_abbr_not_allowed_by_pylint.write(
+            update)
 
 
 def update_result():
@@ -426,4 +433,3 @@ if __name__ == '__main__':
     flag = compare_all()
     if not flag:
         exit(1)
-
