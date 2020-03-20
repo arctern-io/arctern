8f8f4a9cc17432cd3165b07bec3b11be8fea531d
y.d.s.l <exceptionliu@gmail.com>
2020-03-20 18:15:31 +0800

    fuck pylint

--- tests/compare.py
+++ tests/compare.py
@@ -121,10 +121,10 @@ EPOCH_CURVE_RELATIVE = 1e-2
 EPOCH_SURFACE_RELATIVE = 1e-2
 
 
-def compare_length(x, y):
+def compare_length(geometry_x, geometry_y):
     """Compare length of 2 geometry types."""
-    arct = CreateGeometryFromWkt(x)
-    pgis = CreateGeometryFromWkt(y)
+    arct = CreateGeometryFromWkt(geometry_x)
+    pgis = CreateGeometryFromWkt(geometry_y)
 
     intersection_length = Geometry.Length(Geometry.Intersection(arct, pgis))
     arct_length = Geometry.Length(arct)
@@ -138,10 +138,10 @@ def compare_length(x, y):
     return result
 
 
-def compare_area(x, y):
+def compare_area(geometry_x, geometry_y):
     """Compare area of 2 geometry types."""
-    arct = CreateGeometryFromWkt(x)
-    pgis = CreateGeometryFromWkt(y)
+    arct = CreateGeometryFromWkt(geometry_x)
+    pgis = CreateGeometryFromWkt(geometry_y)
 
     intersection_area = Geometry.Area(Geometry.Intersection(arct, pgis))
     arct_area = Geometry.Area(arct)
@@ -155,51 +155,52 @@ def compare_area(x, y):
     return result
 
 
-def compare_geometry(c, x, y):
+def compare_geometry(config, geometry_x, geometry_y):
     """Compare whether 2 geometries is 'equal'."""
 
-    if x.upper().endswith('EMPTY') and y.upper().endswith('EMPTY'):
+    if geometry_x.upper().endswith('EMPTY') and geometry_y.upper().endswith('EMPTY'):
         return True
 
-    if c in BLIST:
-        if arc_distance(x, y) < EPOCH_CURVE_RELATIVE:
+    if config in BLIST:
+        if arc_distance(geometry_x, geometry_y) < EPOCH_CURVE_RELATIVE:
             return True
         else:
-            print('arc distance: %s' % str(arc_distance(x, y)))
+            print('arc distance: %s' %
+                  str(arc_distance(geometry_x, geometry_y)))
             return False
     else:
-        arct = wkt.loads(x)
-        pgis = wkt.loads(y)
+        arct = wkt.loads(geometry_x)
+        pgis = wkt.loads(geometry_y)
         result = arct.equals_exact(pgis, EPOCH)
         return result
 
 
-def compare_geometrycollection(c, x, y):
+def compare_geometrycollection(config, geometry_x, geometry_y):
     """Compare whether 2 geometrycollections is 'equal'."""
-    if c in BLIST:
+    if config in BLIST:
         # print('arc distance: %s' % str(arc_distance(x, y)))
-        return arc_distance(x, y) < EPOCH_CURVE_RELATIVE
+        return arc_distance(geometry_x, geometry_y) < EPOCH_CURVE_RELATIVE
         #     return True
         # else:
         #     print('arc distance: %s' % str(arc_distance(x, y)))
         #     return False
     # else:
 
-    if not c in BLIST:
-        arct = wkt.loads(x)
-        pgis = wkt.loads(y)
+    if not config in BLIST:
+        arct = wkt.loads(geometry_x)
+        pgis = wkt.loads(geometry_y)
         result = arct.equals(pgis)
         return result
 
 
-def compare_floats(c, x, y):
+def compare_floats(config, geometry_x, geometry_y):
     """Compare whether 2 float values is 'equal'."""
-    value_x = float(x)
-    value_y = float(y)
+    value_x = float(geometry_x)
+    value_y = float(geometry_y)
     if value_x == 0:
         return value_y == 0
 
-    if c in ALIST:
+    if config in ALIST:
         precision_error = EPOCH_CURVE_RELATIVE
     else:
         precision_error = EPOCH
@@ -207,12 +208,12 @@ def compare_floats(c, x, y):
     return abs((value_x - value_y)) <= precision_error
 
 
-def compare_float(x, y, z, precision_error):
+def compare_float(geometry_x, geometry_y, geometry_z, precision_error):
     """Compare whether 2 geometries and their intersection is 'equal'."""
 
-    value_x = float(x)
-    value_y = float(y)
-    value_z = float(z)
+    value_x = float(geometry_x)
+    value_y = float(geometry_y)
+    value_z = float(geometry_z)
     return abs((value_x - value_y)) <= precision_error and \
         abs((value_x - value_z)) <= precision_error and \
         abs((value_y - value_z)) <= precision_error
@@ -232,10 +233,10 @@ def compare3float_relative(x_base, y_check, z_intersection, relative_error):
 			compare2float_relative(y_check, z_intersection, relative_error)
 
 
-def compare_curve(x, y):
+def compare_curve(geometry_x, geometry_y):
     """Compare whether 2 curve geometries is 'equal'."""
-    arct = CreateGeometryFromWkt(x)
-    pgis = CreateGeometryFromWkt(y)
+    arct = CreateGeometryFromWkt(geometry_x)
+    pgis = CreateGeometryFromWkt(geometry_y)
 
     intersection_length = Geometry.Length(Geometry.Intersection(arct, pgis))
     arct_length = Geometry.Length(arct)
@@ -246,10 +247,10 @@ def compare_curve(x, y):
     return result
 
 
-def compare_surface(x, y):
+def compare_surface(geometry_x, geometry_y):
     """Compare whether 2 surface geometries is 'equal'."""
-    arct = CreateGeometryFromWkt(x)
-    pgis = CreateGeometryFromWkt(y)
+    arct = CreateGeometryFromWkt(geometry_x)
+    pgis = CreateGeometryFromWkt(geometry_y)
 
     intersection_area = Geometry.Area(Geometry.Intersection(arct, pgis))
     arct_area = Geometry.Area(arct)
