2e09413668d74d9abecf9d96a3ba04a195b885e5
bigsheeper <42060877+bigsheeper@users.noreply.github.com>
2020-03-26 15:56:35 +0800

    add Projection api (#374)
    * use kwargs to wrapper weighted pointmap
    
    * add nyc taxi test
    
    * add nyc taxi test
    
    * fix issue https://github.com/zilliztech/arctern/issues/356
    
    * change api in pyx, remove if else
    
    * add projection api in c++
    
    * add projection api in python and spark
    
    * open render unittest
    
    * add unittest

--- spark/pyspark/arctern_pyspark/_wrapper_func.py
+++ spark/pyspark/arctern_pyspark/_wrapper_func.py
@@ -49,6 +49,7 @@ __all__ = [
     "ST_GeomFromText",
     "ST_GeomFromWKT",
     "ST_AsText",
+    "Projection",
     "TransformAndProjection",
     "WktToWkb",
     "WkbToWkt",
@@ -56,6 +57,10 @@ __all__ = [
 
 import arctern
 from pyspark.sql.functions import pandas_udf, PandasUDFType
+
+@pandas_udf("binary", PandasUDFType.SCALAR)
+def Projection(geos, bottom_right, top_left, height, width):
+    return arctern.projection(geos, bottom_right[0], top_left[0], height[0], width[0])
 
 @pandas_udf("binary", PandasUDFType.SCALAR)
 def TransformAndProjection(geos, src_rs, dst_rs, bottom_right, top_left, height, width):
