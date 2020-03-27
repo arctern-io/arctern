820e4c01d20c6a1b4c497e275027fa6b92a64cd6
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-27 21:13:22 +0800

    Set the imagePullPolicy of the Jenkins slave container to IfNotPresent(default) (#382)
    * Add arctern spark centos version regression
    
    * Add CentOS version of Arctern Spark regression
    
    * Add CentOS version of Arctern Spark regression
    
    * Add CentOS version of Arctern Spark regression
    
    * Add CentOS version of Arctern Spark regression
    
    * Refactor Jenkins CI workflow
    
    * Refactor Jenkins CI workflow
    
    * Set the imagePullPolicy of the Jenkins slave container to default(IfNotPresent)
    
    * Add timeout for whole Jenkins job
    
    * clean up our workspace in Jenkins CI
    
    * clean up our workspace in Jenkins CI

--- ci/jenkins/step/buildArcternSparkImage.groovy
+++ ci/jenkins/step/buildArcternSparkImage.groovy
@@ -1,33 +1,31 @@
-timeout(time: 20, unit: 'MINUTES') {
-    dir ("docker/spark/${BINARY_VERSION}/${OS_NAME}/runtime") {
-        def channelPackage = "conda-bld.tar.gz"
-        def downloadStatus = sh(returnStatus: true, script: "curl -C - -o arctern/${channelPackage} ${ARTFACTORY_URL}/${channelPackage}")
+dir ("docker/spark/${BINARY_VERSION}/${OS_NAME}/runtime") {
+    def channelPackage = "conda-bld.tar.gz"
+    def downloadStatus = sh(returnStatus: true, script: "curl -C - -o arctern/${channelPackage} ${ARTFACTORY_URL}/${channelPackage}")
 
-        if (downloadStatus != 0) {
-            error("\" Download \" ${ARTFACTORY_URL}/${channelPackage} \" failed!")
+    if (downloadStatus != 0) {
+        error("\" Download \" ${ARTFACTORY_URL}/${channelPackage} \" failed!")
+    }
+
+    sh "tar zxvf arctern/${channelPackage} -C ./arctern"
+
+    def baseImageName = "${ARCTERN_REPO}:${OS_NAME}-base"
+    sh "docker pull ${baseImageName}"
+
+    def imageName = "${REPO_NAME}:${TAG_NAME}"
+
+    try {
+        deleteImages("${imageName}", true)
+        def customImage = docker.build("${imageName}", "--build-arg IMAGE_NAME=${ARCTERN_REPO} .")
+        deleteImages("${params.DOKCER_REGISTRY_URL}/${imageName}", true)
+        docker.withRegistry("https://${params.DOKCER_REGISTRY_URL}", "${params.DOCKER_CREDENTIALS_ID}") {
+            customImage.push()
         }
-
-        sh "tar zxvf arctern/${channelPackage} -C ./arctern"
-
-        def baseImageName = "${ARCTERN_REPO}:${OS_NAME}-base"
-        sh "docker pull ${baseImageName}"
-
-        def imageName = "${REPO_NAME}:${TAG_NAME}"
-
-        try {
-            deleteImages("${imageName}", true)
-            def customImage = docker.build("${imageName}", "--build-arg IMAGE_NAME=${ARCTERN_REPO} .")
-            deleteImages("${params.DOKCER_REGISTRY_URL}/${imageName}", true)
-            docker.withRegistry("https://${params.DOKCER_REGISTRY_URL}", "${params.DOCKER_CREDENTIALS_ID}") {
-                customImage.push()
-            }
-        } catch (exc) {
-            throw exc
-        } finally {
-            deleteImages("${imageName}", true)
-            deleteImages("${params.DOKCER_REGISTRY_URL}/${imageName}", true)
-            deleteImages("${baseImageName}", true)
-        }
+    } catch (exc) {
+        throw exc
+    } finally {
+        deleteImages("${imageName}", true)
+        deleteImages("${params.DOKCER_REGISTRY_URL}/${imageName}", true)
+        deleteImages("${baseImageName}", true)
     }
 }
 
