e0ddd20a3921681bb678cae2bd1e7e17a8aed3df
DragonDriver <31589260+DragonDriver@users.noreply.github.com>
2020-03-27 21:12:06 +0800

    test for restful api (#383)
    * gui/server test framework
    
    * pylint check & format code
    
    * gui/server test framework except query
    
    * spark & service module test
    
    * service test fro pointmap
    
    * add /db/query test case
    
    * pylint check & format code
    
    * add small csv to support ci
    
    * empty commit to trigger ci
    
    * tests for restful api
    
    * test for restful api done

---
+++ gui/server/tests/restful/conftest.py
@@ -0,0 +1,51 @@
+"""
+Copyright (C) 2019-2020 Zilliz. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# pylint: disable=redefined-outer-name
+
+import pytest
+import requests
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--host', action='store', default='127.0.0.1', help='the ip address of web server'
+    )
+    parser.addoption(
+        '--port', action='store', default='8080', help='the port of web sever'
+    )
+
+@pytest.fixture
+def host(request):
+    return request.config.getoption('--host')
+
+@pytest.fixture
+def port(request):
+    return request.config.getoption('--port')
+
+@pytest.fixture
+def token(host, port):
+    url = 'http://' + host + ':' + port + '/login'
+    response = requests.post(
+        url=url,
+        json={'username':'zilliz', 'password':'123456'}
+    )
+    return response.json()['data']['token']
+
+@pytest.fixture
+def headers(token):
+    auth_header = {}
+    auth_header['Authorization'] = 'Token ' + str(token)
+    return auth_header
