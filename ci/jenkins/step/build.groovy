f562f25c5ddb0bbce17d087536cc8f0096a620d3
quicksilver <zhifeng.zhang@zilliz.com>
2020-03-27 15:38:28 +0800

    Add timeout for whole Jenkins job

--- ci/jenkins/step/build.groovy
+++ ci/jenkins/step/build.groovy
@@ -1,16 +1,14 @@
-timeout(time: 15, unit: 'MINUTES') {
-    dir ("ci/scripts") {
-        def checkResult = sh(script: "./check_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache", returnStatus: true)
+dir ("ci/scripts") {
+    def checkResult = sh(script: "./check_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache", returnStatus: true)
 
-        if ("${BINARY_VERSION}" == "gpu") {
-            sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"arctern\" -l -g -u --coverage'"
-        } else {
-            sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"arctern\" -l -u --coverage'"
-        }
-        sh "/bin/bash --login -c './python_build.sh -e \"arctern\"'"
+    if ("${BINARY_VERSION}" == "gpu") {
+        sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"arctern\" -l -g -u --coverage'"
+    } else {
+        sh "/bin/bash --login -c './cpp_build.sh -t ${params.BUILD_TYPE} -e \"arctern\" -l -u --coverage'"
+    }
+    sh "/bin/bash --login -c './python_build.sh -e \"arctern\"'"
 
-        withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            def updateResult = sh(script: "./update_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache -u ${USERNAME} -p ${PASSWORD}", returnStatus: true)
-        }
+    withCredentials([usernamePassword(credentialsId: "${params.JFROG_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        def updateResult = sh(script: "./update_ccache.sh -l ${params.JFROG_ARTFACTORY_URL}/ccache -u ${USERNAME} -p ${PASSWORD}", returnStatus: true)
     }
 }
--- ci/jenkins/step/unittest.groovy
+++ ci/jenkins/step/unittest.groovy
@@ -1,6 +1,4 @@
-timeout(time: 5, unit: 'MINUTES') {
-    dir ("ci/scripts") {
-        sh "/bin/bash --login -c './run_unittest.sh -e \"arctern\"'"
-        sh "/bin/bash --login -c './run_pytest.sh -e \"arctern\"'"
-    }
+dir ("ci/scripts") {
+    sh "/bin/bash --login -c './run_unittest.sh -e \"arctern\"'"
+    sh "/bin/bash --login -c './run_pytest.sh -e \"arctern\"'"
 }
