692c3d2980103b3f8fa6e70975322127cf6ef05a
peng.liu <peng.liu@zilliz.com>
2020-03-31 15:53:37 +0800

    add html

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -66,8 +66,8 @@ exclude_patterns = []
 html_last_updated_fmt = '%b %d, %Y'
 html_domain_indices = True
 html_theme = 'sphinx_rtd_theme'
-html_logo = 'static/arctern-color.png'
+#html_logo = 'static/arctern-color.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
