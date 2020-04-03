c90604c7a120457390533c04a9a7500166fccc87
peng.liu <peng.liu@zilliz.com>
2020-04-03 10:29:37 +0800

    change html struct

--- doc/api-doc/source/arctern.rst
+++ doc/api-doc/source/arctern.rst
@@ -1,29 +1,15 @@
 arctern package
 ===============
 
-Subpackages
------------
 
 .. toctree::
 
    arctern.util
 
-Submodules
-----------
-
 arctern.wrapper\_func module
 ----------------------------
 
 .. automodapi:: arctern.wrapper_func
-   :members:
-   :undoc-members:
-   :show-inheritance:
-
-
-Module contents
----------------
-
-.. automodapi:: arctern
    :members:
    :undoc-members:
    :show-inheritance:
--- doc/api-doc/source/conf.py
+++ doc/api-doc/source/conf.py
@@ -62,7 +62,7 @@ exclude_patterns = []
 html_last_updated_fmt = '%b %d, %Y'
 html_domain_indices = True
 html_theme = 'sphinx_rtd_theme'
-html_logo = '../../img/arctern-color.png'
+#html_logo = '../../img/arctern-color.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
