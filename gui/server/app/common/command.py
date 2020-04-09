53b914baa0ca7b5b23dc30c00d19c8b607163d47
dragondriver <longjiquan1998@163.com>
2020-04-09 18:05:06 +0800

    session supported

--- gui/server/app/common/command.py
+++ gui/server/app/common/command.py
@@ -23,10 +23,7 @@ from app.common import error as app_error
 
 API = Blueprint('app_api', __name__)
 
-_DEFAULT_SCOPE = {}
-_COMMAND_SCOPE = {
-    'default': _DEFAULT_SCOPE,
-}
+_COMMAND_SCOPE = {}
 
 @API.errorhandler(app_error.InvalidUsage)
 def invalid_usage(ex):
@@ -58,7 +55,7 @@ def pscope():
 @API.route('/scope/<scope_id>', methods=['DELETE'])
 @token.AUTH.login_required
 def remove_scope():
-    if scope_id not in _COMMAND_SCOPE or scope_id == 'default':
+    if scope_id not in _COMMAND_SCOPE:
         return jsonify(status='error', code=-1, message='scope_id {} not found!'.format(scope_id))
     del _COMMAND_SCOPE[scope_id]
     return jsonify(status='success', code=200, message='remove scope {} successfully!'.format(scope_id))
@@ -69,9 +66,9 @@ def pcommand():
     """
     execute python code with specific scope
     """
-    scope_id = request.json.get('scope_id', default='default')
+    scope_id = request.json.get('scope_id', default=None)
     code = request.json.get('command', None)
-    if scope_id not in _COMMAND_SCOPE:
+    if scope_id is None or scope_id not in _COMMAND_SCOPE:
         return jsonify(status='error', code=-1, message='scope_id {} not found!'.format(scope_id))
     if code is None:
         return jsonify(status='success', code=200, message='execute command successfully!')
@@ -83,3 +80,128 @@ def pcommand():
         raise app_error.InvalidUsage(str(e), 400)
     else:
         return jsonify(status='success', code=200, message='execute command successfully!')
+
+@API.route('/createsession', methods=['POST'])
+@token.AUTH.login_required
+def createsession():
+    uid = str(uuid.uuid1()).replace('-', '')
+    scope_id = request.json.get('scope_id', None)
+    if scope_id is None or scope_id not in _COMMAND_SCOPE:
+        return jsonify(status='error', code=-1, message='scope_id {} not found!'.format(scope_id))
+    scope = _COMMAND_SCOPE[scope_id]
+    session_name = request.json.get('session_name', None)
+    if session_name is None:
+        return jsonify(status='error', code=-1, message='no specific session name!')
+    if session_name in scope.keys():
+        return jsonify(status='error', code=-1, message='session name {} already in use!'.format(session_name))
+    app_name = request.json.get('app_name', 'app' + uid)
+    master_addr = request.json.get('master-addr', default='local[*]')
+    import socket
+    localhost_ip = socket.gethostbyname(socket.gethostname())
+
+    # generate code
+    envs = request.json.get('envs', None)
+    env_code = 'import os\n'
+    keys = ('PYSPARK_PYTHON', 'PYSPARK_DRIVER_PYTHON', 'JAVA_HOME',
+            'HADOOP_CONF_DIR', 'YARN_CONF_DIR', 'GDAL_DATA', 'PROJ_LIB'
+            )
+    if envs:
+        for key in keys:
+            value = envs.get(key, None)
+            if value:
+                env_code += 'os.environ[{}] = {}\n'.format(key, value)
+    print(env_code)
+    session_code = 'from pyspark.sql import SparkSession\n'
+    session_code += '{} = SparkSession.builder'.format(session_name)
+    session_code += '.appName("{}")'.format(app_name)
+    session_code += '.master("{}")'.format(master_addr)
+    session_code += '.config("spark.driver.host", "{}")'.format(localhost_ip)
+    session_code += '.config("spark.sql.execution.arrow.pyspark.enabled", "true")'
+    configs = request.json.get('configs', None)
+    if configs:
+        for key, value in configs.items():
+            if len(value) > 0:
+                session_code += '.config("{}", "{}")'.format(key, value)
+    session_code += '.getOrCreate()'
+    print(session_code)
+    try:
+        exec(env_code, scope)
+        exec(session_code, scope)
+    except Exception as e:
+        raise app_error(str(e), 400)
+    else:
+        return jsonify(status='success', code=200, message='create session successfully!')
+
+@API.route('/closesession', methods=['POST'])
+@token.AUTH.login_required
+def closesession():
+    scope_id = request.json.get('scope_id', None)
+    if scope_id is None or scope_id not in _COMMAND_SCOPE:
+        return jsonify(status='error', code=-1, message='scope_id {} not found!'.format(scope_id))
+    scope = _COMMAND_SCOPE[scope_id]
+    session_name = request.json.get('session_name', None)
+    if session_name is None:
+        return jsonify(status='error', code=-1, message='no specific session name!')
+    if session_name not in scope.keys():
+        return jsonify(status='error', code=-1, message='session name {} not found!'.format(session_name))
+    session_code = '{}.stop()\n'.format(session_name)
+    try:
+        exec(session_code, scope)
+    except Exception as e:
+        raise app_error(str(e), 400)
+    else:
+        return jsonify(status='success', code=200, message='close session successfully!')
+
+@API.route('/loadv2', methods=['POST'])
+@token.AUTH.login_required
+def load_table_v2()
+    scope_id = request.json.get('scope_id', None)
+    if scope_id is None or scope_id not in _COMMAND_SCOPE:
+        return jsonify(status='error', code=-1, message='scope_id {} not found!'.format(scope_id))
+    scope = _COMMAND_SCOPE[scope_id]
+    session_name = request.json.get('session_name', None)
+    if session_name is None:
+        return jsonify(status='error', code=-1, message='no specific session name!')
+    if session_name not in scope.keys():
+        return jsonify(status='error', code=-1, message='session name {} not found!'.format(session_name))
+    
+    # generate code
+    tables = request.json.get('tables', None)
+    for table in tables:
+        table_name = table.get('name')
+        if 'path' in table and 'schema' in table and 'format' in table:
+            options = table.get('options', None)
+
+            schema = str()
+            for column in table.get('schema'):
+                for key, value in column.items():
+                    schema += key + ' ' + value + ', '
+            rindex = schema.rfind(',')
+            schema = schema[:rindex]
+
+            table_format = table.get('format')
+            path = table.get('path')
+            session_code = '{}_df = {}.read'.format(table_name, session_name)
+            session_code += '.format("{}")'.format(table_format)
+            session_code += '.schema("{}")'.format(schema)
+            for key, value in options.items():
+                session_code += '.option("{}", "{}")'.format(key, value)
+            session_code += '.load("{}")\n'.format(path)
+            session_code += '{}_df.createOrReplaceTempView("{}")'.format(table_name, table_name)
+            print(session_code)
+            try:
+                exec(session_code, scope)
+            except Exception as e:
+                raise app_error(str(e), 400)
+            else:
+                return jsonify(status='success', code=200, message='close session successfully!')
+        elif 'sql' in table:
+            sql = table.get('sql')
+            session_code = '{}_df = {}.run("{}")\n'.format(table_name, session_name, sql)
+            session_code += '{}_df.createOrReplaceTempView("{}")'.format(table_name, table_name)
+            try:
+                exec(session_code, scope)
+            except Exception as e:
+                raise app_error(str(e), 400)
+            else:
+                return jsonify(status='success', code=200, message='close session successfully!')
