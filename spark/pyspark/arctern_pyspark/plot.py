817ce7ae3e5f39eb50f16f361ce1e5d2015724a1
yefu.chen <yefu.chen@zilliz.com>
2020-04-12 19:15:13 +0800

    pylint

--- spark/pyspark/arctern_pyspark/plot.py
+++ spark/pyspark/arctern_pyspark/plot.py
@@ -12,38 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# def _plot_spark_data_frame(ax, geoms):
-#     import json
-#     from pyspark.sql.functions import col
-    
-#     if len(geoms.columns) != 1:
-#         raise RuntimeError(f"The input param 'geoms' should have only one column. geoms schema = {geoms.schema}")
-#     head_row = geoms.head()
-#     if head_row==None:
-#         return ax
-#     head_row = head_row[0]
-#     if isinstance(head_row, str): # input is geojson
-#         geoms = geoms.collect()
-#     elif isinstance(head_row,bytearray): #input is wkb
-#         from arctern_pyspark._wrapper_func import ST_AsGeoJSON
-#         geoms = geoms.select(ST_AsGeoJSON(col(geoms.columns[0])))
-#         geoms = geoms.collect()
-#     else:
-#         raise RuntimeError(f"unexpected input type, {type(head_row)}")
-        
-#     plot_collect = dict()
-#     for geo in geoms:
-#         geo_dict = json.loads(geo[0])
-#         _flat_geoms(geo_dict,plot_collect)
-    
-#     _plot_collection(ax, plot_collect)
-
-
-# def plot(ax, geoms):
-#     import pyspark.sql.dataframe
-#     if isinstance(geoms,pyspark.sql.dataframe.DataFrame):
-#         _plot_spark_data_frame(ax, geoms)
-
 def plot(ax, geoms):
     import pyspark.sql.dataframe
     import arctern
