20f78005130fa6399a2ce49b893bb913d3283b74
zhen.an <zhen.an@zilliz.com>
2020-04-13 10:24:26 +0800

    add client submodule

--- gui/client/src/pages/Page404.tsx
+++
@@ -1,9 +0,0 @@
-import React, {useContext} from 'react';
-import {I18nContext} from '../contexts/I18nContext';
-
-const Page404: React.FC = () => {
-  const {nls} = useContext(I18nContext);
-  return <div className="404">{nls.label_404}</div>;
-};
-
-export default Page404;
--- gui/client/src/widgets/NumberChart/view.tsx
+++
@@ -1,9 +0,0 @@
-import React, {FC} from 'react';
-import NumberChart from '../NumberChart';
-import {DefaultWidgetProps} from '../../types';
-
-const NumberChartView: FC<DefaultWidgetProps> = props => {
-  return <NumberChart {...props} />;
-};
-
-export default NumberChartView;
