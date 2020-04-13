3bace259a41f62df09fd5ad3454484e134bcea0e
AzAz <adam_an02@163.com>
2020-04-13 15:32:28 +0800

    add client submodule (#484)
    Co-authored-by: zhen.an <zhen.an@zilliz.com>

--- gui/client/src/components/common/selectors/index.ts
+++
@@ -1,2 +0,0 @@
-export {default as QuerySelector} from './QuerySelector';
-export {default as SimpleSelector} from './SimpleSelector';
--- gui/client/src/widgets/GeoHeatMap/types.ts
+++
@@ -1,4 +0,0 @@
-import {MapChartConfig, MapChartProps} from '../common/MapChart.type';
-
-export type GeoHeatMapProps = MapChartProps;
-export type GeoHeatMapConfig = MapChartConfig;
