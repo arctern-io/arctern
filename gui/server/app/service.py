d4870a601abdeb9200e8b886705fde2ab46e23a5
bigsheeper <42060877+bigsheeper@users.noreply.github.com>
2020-04-13 16:59:37 +0800

    Server catch exception (#486)
    * fix wrong choroplethmap when png size is small
    
    * fix wrong choroplethmap when png size is small
    
    * add test for transform_projection
    
    * format projection test
    
    * add exception handler
    
    * write exception to log
    
    * return 200, and use error level log
    
    Co-authored-by: xi.ge <xi.ge@zilliz.com>

--- gui/server/app/service.py
+++ gui/server/app/service.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 
 import json
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, make_response
 
 from arctern.util.vega import vega_choroplethmap, vega_heatmap, vega_pointmap, vega_weighted_pointmap
 from arctern_pyspark import choroplethmap, heatmap, pointmap, weighted_pointmap
@@ -24,6 +24,11 @@ from app import account
 from app.common import spark, token, utils, db, log
 
 API = Blueprint('app_api', __name__)
+
+@API.errorhandler(Exception)
+def exception_handler(e):
+    log.INSTANCE.error('exception: {}'.format(str(e)))
+    return jsonify(status='error', code=-1, message=str(e))
 
 def load_data(content):
     if not utils.check_json(content, 'db_name') \
@@ -245,4 +250,5 @@ def db_query():
             return jsonify(status="error",
                            code=-1,
                            message='{} not support'.format(query_type))
+
     return jsonify(status="success", code=200, data=content)
