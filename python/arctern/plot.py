0ed51b6530471fb690dd41f82644d44fc852a0ad
yefu.chen <yefu.chen@zilliz.com>
2020-04-14 10:23:48 +0800

    pylint

--- python/arctern/plot.py
+++ python/arctern/plot.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import random
+import json
 import numpy as np
 import pandas.core.series
 import arctern
