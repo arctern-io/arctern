dfca2f854d623297d5cc595c846a5e5cbb702c5d
yefu.chen <yefu.chen@zilliz.com>
2020-04-14 10:28:30 +0800

    plot style

--- spark/pyspark/arctern_pyspark/plot.py
+++ spark/pyspark/arctern_pyspark/plot.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-def plot(ax, geoms):
+def plot(ax, geoms, **style_kwds):
     import pyspark.sql.dataframe
     import arctern
     if isinstance(geoms, pyspark.sql.dataframe.DataFrame):
         pandas_df = geoms.toPandas()
-        arctern.plot(ax, pandas_df)
+        arctern.plot(ax, pandas_df, **style_kwds)
