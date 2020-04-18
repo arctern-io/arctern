1a5dcb9f55e562e1111351b670cd1326f91f1149
become-nice <995581097@qq.com>
2020-04-18 17:44:11 +0800

    ...

--- spark/pyspark/arctern_pyspark/_wrapper_func.py
+++ spark/pyspark/arctern_pyspark/_wrapper_func.py
@@ -1,4 +1,4 @@
-# Copygeo2 (C) 2019-2020 Zilliz. All geo2s reserved.
+# Copygeo2 (C) 2019-2020 Zilliz. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
