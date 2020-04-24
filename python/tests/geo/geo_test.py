ebc3db0a333700fa76e64ff96261558d01a9c8dd
become-nice <995581097@qq.com>
2020-04-24 17:42:52 +0800

    fix api comment

--- python/tests/geo/geo_test.py
+++ python/tests/geo/geo_test.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import arctern
 import pandas
 from osgeo import ogr
-import arctern
 
 
 def test_ST_IsValid():
