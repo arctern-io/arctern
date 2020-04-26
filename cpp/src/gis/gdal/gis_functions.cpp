ad9ec1e531d214b6156c1635b3033bb68239e1a4
yefu.chen <yefu.chen@zilliz.com>
2020-04-26 21:23:23 +0800

    clang-format

--- cpp/src/gis/gdal/gis_functions.cpp
+++ cpp/src/gis/gdal/gis_functions.cpp
@@ -239,15 +239,14 @@ bool GetNextValue(const std::vector<std::shared_ptr<arrow::Array>>& chunk_array,
 }
 
 template <typename T>
-bool GetNextValue(std::vector<std::vector<std::shared_ptr<arrow::Array>>> &array_list,
-                  std::vector<ChunkArrayIdx<T>> &idx_list,
-                  bool &is_null){
+bool GetNextValue(std::vector<std::vector<std::shared_ptr<arrow::Array>>>& array_list,
+                  std::vector<ChunkArrayIdx<T>>& idx_list, bool& is_null) {
   auto ret_val = GetNextValue(array_list[0], idx_list[0]);
   is_null = idx_list[0].is_null;
 
-  for(int i=1; i<array_list.size(); ++i){
+  for (int i = 1; i < array_list.size(); ++i) {
     auto cur_val = GetNextValue(array_list[i], idx_list[i]);
-    if(cur_val != ret_val){
+    if (cur_val != ret_val) {
       throw std::runtime_error("incorrect input data");
     }
     is_null |= idx_list[i].is_null;
@@ -260,7 +259,8 @@ bool GetNextValue(std::vector<std::vector<std::shared_ptr<arrow::Array>>> &array
 std::vector<std::shared_ptr<arrow::Array>> ST_Point(
     const std::vector<std::shared_ptr<arrow::Array>>& x_values_raw,
     const std::vector<std::shared_ptr<arrow::Array>>& y_values_raw) {
-  std::vector<std::vector<std::shared_ptr<arrow::Array>>> array_list{x_values_raw, y_values_raw};
+  std::vector<std::vector<std::shared_ptr<arrow::Array>>> array_list{x_values_raw,
+                                                                     y_values_raw};
   std::vector<ChunkArrayIdx<double>> idx_list(2);
 
   OGRPoint point;
@@ -268,10 +268,10 @@ std::vector<std::shared_ptr<arrow::Array>> ST_Point(
   std::vector<std::shared_ptr<arrow::Array>> result_array;
   bool is_null;
 
-  while(GetNextValue(array_list, idx_list, is_null)){
-    if(is_null){
+  while (GetNextValue(array_list, idx_list, is_null)) {
+    if (is_null) {
       builder.array_builder.AppendNull();
-    }else{
+    } else {
       point.setX(idx_list[0].item_value);
       point.setY(idx_list[1].item_value);
       auto array_ptr = AppendWkb(builder, &point);
