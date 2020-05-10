1f32bf8278c77d552e0b588657086a24c7bc351e
fluorinedog <fluorinedog@gmail.com>
2020-05-10 22:08:06 +0800

    update

--- cpp/src/gis/dispatch/aligned_execute.h
+++ cpp/src/gis/dispatch/aligned_execute.h
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 #pragma once
 #include <memory>
 #include <vector>
@@ -11,7 +28,7 @@ namespace dispatch {
 using ArrayType = arrow::BinaryArray;
 class ArrowVectorAlignedSlicer {
  public:
-  static constexpr size_t slice_limit = 10L * 1000L * 1000L; // 10 million
+  static constexpr size_t slice_limit = 10L * 1000L * 1000L;  // 10 million
   void Register(const std::vector<ArrayPtr>& vec) {
     size_t sum = 0;
     for (const auto& arr : vec) {
@@ -23,8 +40,8 @@ class ArrowVectorAlignedSlicer {
   void Format() {
     arr_indexes_.erase(0);
     size_t last_index = 0;
-    for(auto index: arr_indexes_) {
-      while(index - last_index > slice_limit) {
+    for (auto index : arr_indexes_) {
+      while (index - last_index > slice_limit) {
         last_index += slice_limit;
         arr_indexes_.insert(last_index);
       }
@@ -33,7 +50,7 @@ class ArrowVectorAlignedSlicer {
   }
 
   std::vector<ArrayPtr> Slice(const std::vector<ArrayPtr>& raw_arrow_vector) const {
-    if(arr_indexes_.size() == 1 && raw_arrow_vector.size() == 1) {
+    if (arr_indexes_.size() == 1 && raw_arrow_vector.size() == 1) {
       // fast path
       return raw_arrow_vector;
     }
@@ -66,7 +83,7 @@ class ArrowVectorAlignedSlicer {
 
 template <typename Func>
 std::vector<ArrayPtr> AlignedExecuteBinary(Func functor, std::vector<ArrayPtr> raw_input1,
-                                    std::vector<ArrayPtr> raw_input2) {
+                                           std::vector<ArrayPtr> raw_input2) {
   dispatch::ArrowVectorAlignedSlicer slicer;
   slicer.Register(raw_input1);
   slicer.Register(raw_input2);
