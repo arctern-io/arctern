3a85d4bfb52509adc0db349a4bbd4113651336e9
DragonDriver <longjiquan1998@163.com>
2020-05-18 23:34:58 +0800

    trigger ci

--- gui/server/arctern_server/tests/restful/test_function.py
+++ gui/server/arctern_server/tests/restful/test_function.py
@@ -14,3 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+# pylint: disable=redefined-outer-name
+
+def test_hello_world():
+    print("TODO: add test case for zeppelin interface!")
