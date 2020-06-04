df2b9fb7042d312f2e27cf368ea2f3d62e596d1e
bigsheeper <42060877+bigsheeper@users.noreply.github.com>
2020-06-04 10:40:37 +0800

    wrap python render api

--- python/arctern/_wrapper_func.py
+++ python/arctern/_wrapper_func.py
@@ -1373,12 +1373,7 @@ def point_map_layer(vega, points, transform=True):
 
     vega_string = vega.build().encode('utf-8')
     rs = arctern_core_.point_map(vega_string, geos_rs)
-    rs = rs.to_pandas()
-    print("get here^&&&&&&&&&&&&&&&&&&&&")
-    print(type(rs))
-    print(type(rs[0]))
-    print(rs[0])
-    return base64.b64encode(rs[0])
+    return base64.b64encode(rs.to_pandas()[0])
 
 
 # pylint: disable=too-many-branches
@@ -1446,7 +1441,7 @@ def weighted_point_map_layer(vega, points, transform=True, **kwargs):
         color_weights_rs = _to_arrow_array_list(arr_c)
         rs = arctern_core_.weighted_color_point_map(vega_string, geos_rs, color_weights_rs)
 
-    return base64.b64encode(bytes(rs))
+    return base64.b64encode(rs.to_pandas()[0])
 
 
 def heat_map_layer(vega, points, weights, transform=True):
@@ -1488,7 +1483,7 @@ def heat_map_layer(vega, points, weights, transform=True):
 
     vega_string = vega.build().encode('utf-8')
     rs = arctern_core_.heat_map(vega_string, geos_rs, weights_rs)
-    return base64.b64encode(bytes(rs))
+    return base64.b64encode(rs.to_pandas()[0])
 
 
 def choropleth_map_layer(vega, region_boundaries, weights, transform=True):
@@ -1531,7 +1526,7 @@ def choropleth_map_layer(vega, region_boundaries, weights, transform=True):
     weights_rs = _to_arrow_array_list(arr)
 
     rs = arctern_core_.choropleth_map(vega_string, geos_rs, weights_rs)
-    return base64.b64encode(bytes(rs))
+    return base64.b64encode(rs.to_pandas()[0])
 
 
 def icon_viz_layer(vega, points, transform=True):
@@ -1566,7 +1561,7 @@ def icon_viz_layer(vega, points, transform=True):
     vega_string = vega.build().encode('utf-8')
 
     rs = arctern_core_.icon_viz(vega_string, geos_rs)
-    return base64.b64encode(bytes(rs))
+    return base64.b64encode(rs.to_pandas()[0])
 
 
 def fishnet_map_layer(vega, points, weights, transform=True):
@@ -1608,7 +1603,7 @@ def fishnet_map_layer(vega, points, weights, transform=True):
 
     vega_string = vega.build().encode('utf-8')
     rs = arctern_core_.fishnet_map(vega_string, geos_rs, weights_rs)
-    return base64.b64encode(bytes(rs))
+    return base64.b64encode(rs.to_pandas()[0])
 
 def nearest_location_on_road(roads, points):
     import pyarrow as pa
