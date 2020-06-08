10752e887b6ce07edb74cc55bae862ed30edafbb
xiaocai2333 <46207236+xiaocai2333@users.noreply.github.com>
2020-06-08 11:46:50 +0800

    Branch 0.2.x (#2)
    * Add commen for GeoSeries.fillna (#713)
    
    * supplement example
    
    * test
    
    * supplement example
    
    * add comment for geoseries.fillna
    
    * turn off axis (#714)
    
    * [skip ci]Remove ST and format polygon (#717)
    
     [skip ci] remove st func in comment
    
    * [skip ci] modify plot_geometry comments (#718)
    
    * [skip ci] add example for crs (#720)
    
    * [skip ci] add docs for plot with map
    
    [skip ci] add docs for plot with map
    
    * [skip ci] fix typo (#722)
    
    [skip ci] fix typo (#722)
    
    * [skip ci] remove spacing (#723)
    
    [skip ci] remove spacing
    
    Co-authored-by: FluorineDog <fluorinedog@gmail.com>
    Co-authored-by: bigsheeper <42060877+bigsheeper@users.noreply.github.com>
    Co-authored-by: czp <2426009680@qq.com>
    Co-authored-by: shengjh <46514371+shengjh@users.noreply.github.com>

--- python/arctern/plot/_plot.py
+++ python/arctern/plot/_plot.py
@@ -292,21 +292,21 @@ def plot_geometry(ax, geoms, **style_kwds):
        >>> raw_data.append('linestring(0 10, 5 5, 10 0)')
        >>> raw_data.append('polygon((2 2,2 3,3 3,3 2,2 2))')
        >>> raw_data.append("GEOMETRYCOLLECTION("
-                           "polygon((1 1,1 2,2 2,2 1,1 1)),"
-                           "linestring(0 1, 5 6, 10 11),"
-                           "POINT(4 7))")
+       ...                 "polygon((1 1,1 2,2 2,2 1,1 1)),"
+       ...                 "linestring(0 1, 5 6, 10 11),"
+       ...                 "POINT(4 7))")
        >>> arr_wkt = pandas.Series(raw_data)
-       >>> arr_wkb = arctern.ST_CurveToLine(arctern.ST_GeomFromText(arr_wkt))
+       >>> arr_wkb = (arctern.GeoSeries(arr_wkt)).curve_to_line()
        >>> df = pandas.DataFrame({'wkb':arr_wkb})
        >>> fig, ax = plt.subplots()
        >>> arctern.plot.plot_geometry(ax, df,
-                                 color=['orange', 'green', 'blue', 'red'],
-                                 marker='^',
-                                 markersize=100,
-                                 linewidth=[None, 7, 8, 5],
-                                 linestyle=[None, 'dashed', 'dashdot', None],
-                                 edgecolor=[None, None, 'red', None],
-                                 facecolor=[None, None, 'black', None])
+       ...                            color=['orange', 'green', 'blue', 'red'],
+       ...                            marker='^',
+       ...                            markersize=100,
+       ...                            linewidth=[None, 7, 8, 5],
+       ...                            linestyle=[None, 'dashed', 'dashdot', None],
+       ...                            edgecolor=[None, None, 'red', None],
+       ...                            facecolor=[None, None, 'black', None])
        >>> ax.grid()
        >>> fig.savefig('/tmp/plot_test.png')
     """
