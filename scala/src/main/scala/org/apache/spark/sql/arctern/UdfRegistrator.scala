23fc16978f10fe3183f2465af93abd853eefdcc3
bigsheeper <42060877+bigsheeper@users.noreply.github.com>
2020-06-12 17:05:24 +0800

    add ST_Contains

--- scala/src/main/scala/org/apache/spark/sql/arctern/UdfRegistrator.scala
+++ scala/src/main/scala/org/apache/spark/sql/arctern/UdfRegistrator.scala
@@ -33,5 +33,6 @@ object UdfRegistrator {
     spark.sessionState.functionRegistry.createOrReplaceTempFunction("ST_Touches", ST_Touches)
     spark.sessionState.functionRegistry.createOrReplaceTempFunction("ST_Overlaps", ST_Overlaps)
     spark.sessionState.functionRegistry.createOrReplaceTempFunction("ST_Crosses", ST_Crosses)
+    spark.sessionState.functionRegistry.createOrReplaceTempFunction("ST_Contains", ST_Contains)
   }
 }
--- scala/src/main/scala/org/apache/spark/sql/arctern/expressions/Functions.scala
+++ scala/src/main/scala/org/apache/spark/sql/arctern/expressions/Functions.scala
@@ -478,3 +478,15 @@ case class ST_Crosses(inputsExpr: Seq[Expression]) extends ST_BinaryOp {
 
   override def dataType: DataType = BooleanType
 }
+
+case class ST_Contains(inputsExpr: Seq[Expression]) extends ST_BinaryOp {
+  assert(inputsExpr.length == 2)
+
+  override def leftExpr: Expression = inputsExpr.head
+
+  override def rightExpr: Expression = inputsExpr(1)
+
+  override protected def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = codeGenJob(ctx, ev, (left, right) => s"$left.contains($right)")
+
+  override def dataType: DataType = BooleanType
+}
