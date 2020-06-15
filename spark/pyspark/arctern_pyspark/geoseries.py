56884f0cba4d9c970482cea5b36573cebb6da6a9
shengjh <1572099106@qq.com>
2020-06-15 20:27:14 +0800

    support geoseries unary operation with index

--- spark/pyspark/arctern_pyspark/geoseries.py
+++ spark/pyspark/arctern_pyspark/geoseries.py
@@ -1,16 +1,29 @@
 import arctern
+import databricks.koalas as ks
+import pandas as pd
+from databricks.koalas import DataFrame, get_option
+from databricks.koalas.base import IndexOpsMixin
+from databricks.koalas.internal import _InternalFrame
+from databricks.koalas.series import REPR_PATTERN
+from pandas.io.formats.printing import pprint_thing
 
 # os.environ['PYSPARK_PYTHON'] = "/home/shengjh/miniconda3/envs/koalas/bin/python"
 # os.environ['PYSPARK_DRIVER_PYTHON'] = "/home/shengjh/miniconda3/envs/koalas/bin/python"
 
-import databricks.koalas as ks
-from databricks.koalas import DataFrame, get_option
-from databricks.koalas.base import IndexOpsMixin
-from databricks.koalas.internal import _InternalFrame
-from databricks.koalas.series import REPR_PATTERN, _col
-from pandas.io.formats.printing import pprint_thing
-
 ks.set_option('compute.ops_on_diff_frames', True)
+
+
+# for unary operation, which return a koalas series.
+def _unary_op(kss, f):
+    from arctern_pyspark import _wrapper_func
+    _kdf = kss.to_dataframe()
+    ret_col = getattr(_wrapper_func, f)(kss.spark_column)
+    return ks.Series(
+        _kdf._internal.copy(
+            spark_column=ret_col, column_labels=kss._internal.column_labels
+        ),
+        anchor=_kdf
+    )
 
 
 class GeoSeries(ks.Series):
@@ -31,13 +44,13 @@ class GeoSeries(ks.Series):
                 assert name is None
                 assert not copy
                 assert not fastpath
-                s = data.astype(object, copy=False)
+                pds = data.astype(object, copy=False)
             else:
-                s = arctern.GeoSeries(
+                pds = arctern.GeoSeries(
                     data=data, index=index, dtype=dtype, name=name, copy=copy, fastpath=fastpath
                 )
-                s = s.astype(object, copy=False)
-            kdf = DataFrame(s)
+                pds = pds.astype(object, copy=False)
+            kdf = DataFrame(pds)
             IndexOpsMixin.__init__(
                 self, kdf._internal.copy(spark_column=kdf._internal.data_spark_columns[0]), kdf
             )
@@ -45,9 +58,18 @@ class GeoSeries(ks.Series):
     def __repr__(self):
         max_display_count = get_option("display.max_rows")
         if max_display_count is None:
-            return arctern.ST_AsText(self._to_internal_pandas()).to_string(name=self.name, dtype=self.dtype)
+            pser = pd.Series(arctern.ST_AsText(self._to_internal_pandas()).to_numpy(),
+                             name=self.name,
+                             index=self.index.to_numpy(),
+                             copy=False
+                             )
+            return pser.to_string(name=self.name, dtype=self.dtype)
 
-        pser = arctern.ST_AsText(self.head(max_display_count + 1)._to_internal_pandas())
+        pser = pd.Series(arctern.ST_AsText(self.head(max_display_count + 1)._to_internal_pandas()).to_numpy(),
+                         name=self.name,
+                         index=self.index.to_numpy(),
+                         copy=False
+                         )
         pser_length = len(pser)
         pser = pser.iloc[:max_display_count]
         if pser_length > max_display_count:
@@ -65,34 +87,24 @@ class GeoSeries(ks.Series):
 
     @property
     def area(self):
-        from arctern_pyspark import _wrapper_func
-        from pyspark.sql.functions import col
-
-        _kdf = self.to_dataframe()
-        sdf = _kdf.to_spark()
-
-        ret = sdf.select(_wrapper_func.ST_Area(col(self.name)).alias(self.name))  # spark dataframe
-
-        kdf = ret.to_koalas()
-        return kdf[self.name]
-
-        # from databricks.koalas.internal import _InternalFrame
-        # internal = _InternalFrame(
-        #     kdf._internal.spark_frame,
-        #     index_map=_kdf._internal.index_map,
-        #     column_labels=_kdf._internal.column_labels,
-        #     column_label_names=_kdf._internal.column_label_names,
-        # )
-        # return ks.Series(internal, anchor=kdf)
+        return _unary_op(self, "ST_Area")
 
 
-rows = 1
+if __name__ == '__main__':
+    rows = 1000000
+    data_series = [
+                      'POLYGON ((1 1,1 2,2 2,2 1,1 1))',
+                      'POLYGON ((0 0,0 4,2 2,4 4,4 0,0 0))',
+                      'POLYGON ((0 0,0 4,4 4,0 0))',
+                  ] * rows
 
-data_series = [
-                  'POLYGON ((1 1,1 2,2 2,2 1,1 1))',
-                  'POLYGON ((0 0,0 4,2 2,4 4,4 0,0 0))',
-                  'POLYGON ((0 0,0 4,4 4,0 0))',
-              ] * rows
+    countries = ['London', 'New York', 'Helsinki'] * rows
+    s = ks.Series(data_series, name="haha", index=countries)
+    s1 = ks.Series(data_series, name="haha", index=countries)
 
-countries = ['London', 'New York', 'Helsinki'] * rows
-s = ks.Series(data_series, name="haha", index=countries)
+    s2 = GeoSeries(data_series, name="haha", index=countries)
+
+    # print(s)
+    # print(s2)
+    print(s2.area)
+    # print(s1+ s)
