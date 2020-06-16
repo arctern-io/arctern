2f84c3935964af51f4c3b69006140d33b6e9a8e8
beautifulpython <2426009680@qq.com>
2020-06-16 16:06:58 +0800

    add ST_Translate unittest

--- cpp/unittest/gis/gis_functions_tests.cpp
+++ cpp/unittest/gis/gis_functions_tests.cpp
@@ -4098,3 +4098,41 @@ for(int i=0;i<res_str->length();++i){
  ASSERT_EQ(res_str->GetString(6), "GEOMETRYCOLLECTION (POLYGON ((0 0,1 4,1 0,0 0)),POINT (1 5))");
 }
 
+TEST(geometry_test, test_ST_Translate) {
+auto p1 = "POINT (0 1)";
+auto p2 = "LINESTRING (0 0, 0 1, 1 1)";
+auto p3 = "LINESTRING (0 0, 1 0, 1 1, 0 0)";
+auto p4 = "POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))";
+auto p5 = "MULTIPOINT (0 0, 1 0, 1 2, 1 2)";
+auto p6 = "MULTILINESTRING ( (0 0, 1 2), (0 0, 1 0, 1 1),(-1 2,3 4,1 -3,-2 1) )";
+auto p7 = "MULTIPOLYGON ( ((0 0, 1 4, 1 0,0 0)) )";
+
+arrow::StringBuilder builder;
+std::shared_ptr<arrow::Array> input;
+builder.Append(std::string(p1));
+builder.Append(std::string(p2));
+builder.Append(std::string(p3));
+builder.Append(std::string(p4));
+builder.Append(std::string(p5));
+builder.Append(std::string(p6));
+builder.Append(std::string(p7));
+builder.Finish(&input);
+
+// temp solution bcz ST_GeomFromText and ST_AsText do not have Implementation based on ChunkedArray.
+auto wkb_chunks = arctern::gis::ST_GeomFromText(input);
+auto wkb_chunked_array = std::make_shared<arrow::ChunkedArray>(std::move(wkb_chunks));
+auto wkb_res = arctern::gis::ST_Translate(wkb_chunked_array,1.2,-3);
+auto res = arctern::gis::ST_AsText(*wkb_res->chunks().data());
+auto res_str = std::static_pointer_cast<arrow::StringArray>(*res.data());
+
+for(int i=0;i<res_str->length();++i){
+  std::cout<<res_str->GetString(i)<<std::endl;
+}
+ASSERT_EQ(res_str->GetString(0), "POINT (1.2 -2.0)");
+ASSERT_EQ(res_str->GetString(1), "LINESTRING (1.2 -3.0,1.2 -2.0,2.2 -2.0)");
+ASSERT_EQ(res_str->GetString(2), "LINESTRING (1.2 -3.0,2.2 -3.0,2.2 -2.0,1.2 -3.0)");
+ASSERT_EQ(res_str->GetString(3), "POLYGON ((1.2 -3.0,2.2 -3.0,2.2 -2.0,1.2 -2.0,1.2 -3.0))");
+ASSERT_EQ(res_str->GetString(4), "MULTIPOINT (1.2 -3.0,2.2 -3.0,2.2 -1.0,2.2 -1.0)");
+ASSERT_EQ(res_str->GetString(5), "MULTILINESTRING ((1.2 -3.0,2.2 -1.0),(1.2 -3.0,2.2 -3.0,2.2 -2.0),(0.2 -1.0,4.2 1.0,2.2 -6.0,-0.8 -2.0))");
+ASSERT_EQ(res_str->GetString(6), "MULTIPOLYGON (((1.2 -3.0,2.2 1.0,2.2 -3.0,1.2 -3.0)))");
+}
