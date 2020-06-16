b831e1db0f613d983eea982ae434a7d693b3c669
czs007 <czs@alumni.hust.edu.cn>
2020-06-16 15:06:13 +0800

    clang format

--- cpp/src/gis/api.h
+++ cpp/src/gis/api.h
@@ -120,12 +120,12 @@ std::shared_ptr<arrow::ChunkedArray> ST_IsEmpty(
     const std::shared_ptr<arrow::ChunkedArray>& geometries);
 
 std::shared_ptr<arrow::ChunkedArray> ST_Scale(
-    const std::shared_ptr<arrow::ChunkedArray>& geometries,
-    double factor_x, double factor_y);
+    const std::shared_ptr<arrow::ChunkedArray>& geometries, double factor_x,
+    double factor_y);
 
 std::shared_ptr<arrow::ChunkedArray> ST_Affine(
-  const std::shared_ptr<arrow::ChunkedArray>& geometries,
-  double a, double b, double d, double e, double offset_x, double offset_y);
+    const std::shared_ptr<arrow::ChunkedArray>& geometries, double a, double b, double d,
+    double e, double offset_x, double offset_y);
 std::shared_ptr<arrow::ChunkedArray> ST_Union(
     const std::shared_ptr<arrow::ChunkedArray>& geometries_1,
     const std::shared_ptr<arrow::ChunkedArray>& geometries_2);
