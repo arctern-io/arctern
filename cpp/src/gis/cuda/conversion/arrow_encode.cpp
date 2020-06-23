0d936530653826318c3e2edc4b75e8c16b922816
czs007 <czs@alumni.hust.edu.cn>
2020-06-23 15:26:22 +0800

    fix bug

--- cpp/src/gis/cuda/conversion/arrow_encode.cpp
+++ cpp/src/gis/cuda/conversion/arrow_encode.cpp
@@ -30,8 +30,14 @@ namespace {
 
 template <typename T>
 std::shared_ptr<arrow::Buffer> AllocArrowBufferAndCopy(int size, const T* dev_ptr) {
+  std::shared_ptr<arrow::Buffer> buffer;
   auto len = sizeof(T) * size;
-  auto buffer = arrow::AllocateBuffer(len);
+  auto result = arrow::AllocateBuffer(len);
+  if (result.ok()) {
+    buffer = std::move(result).ValueOrDie();	
+  } else {
+    throw std::runtime_error(result.status().message());    
+  }
   GpuMemcpy((T*)buffer->mutable_data(), dev_ptr, size);
   return buffer;
 }
