5ae52a7e534de2d611d55d7d49672abe4bdca120
become-nice <995581097@qq.com>
2020-06-30 16:54:49 +0800

    Add test

--- python/arctern/geodataframe/geodataframe.py
+++ python/arctern/geodataframe/geodataframe.py
@@ -14,11 +14,11 @@
 
 # pylint: disable=too-many-lines
 # pylint: disable=too-many-public-methods, unused-argument, redefined-builtin
-
-from warnings import warn
-
-from pandas import DataFrame,Series
+import json
+from pandas import DataFrame, Series
 from arctern import GeoSeries
+import pandas as pd
+import numpy as np
 
 
 class GeoDataFrame(DataFrame):
@@ -31,6 +31,7 @@ class GeoDataFrame(DataFrame):
         crs = kwargs.pop("crs", None)
         geometries = kwargs.pop("geometries", None)
         super(GeoDataFrame, self).__init__(*args, **kwargs)
+        print(self[0].crs)
 
         if geometries is None and "geometry" in self.columns:
             index = self.index
@@ -82,23 +83,90 @@ class GeoDataFrame(DataFrame):
 
         geometry_cols = frame._geometry_column_name
         for col in cols:
-            if col not in geometry_cols:
+            if col not in geometry_cols and not isinstance(frame[col], GeoSeries):
                 frame[col] = GeoSeries(frame[col])
 
         frame._geometry_column_name = frame._geometry_column_name + cols
 
         for col in frame._geometry_column_name:
-            frame[col].set_crs(geos_crs[col])
+            if frame[col].crs is None:
+                frame[col].set_crs(geos_crs[col])
 
         if not inplace:
             return frame
 
+    def to_json(self, na="null", show_bbox=False, col='geometry', **kwargs):
+        return json.dumps(self._to_geo(na=na, show_bbox=show_bbox), **kwargs)
+
+    def _to_geo(self, **kwargs):
+        geo = {
+            "type": "FeatureCollection",
+            "features": list(self.iterfeatures(**kwargs))
+        }
+
+        if kwargs.get("show_bbox", False):
+            geo["bbox"] = self[kwargs.get("col")].envelope_aggr()
+
+        return geo
+
+    def iterfeatures(self, na="null", show_bbox=False, col='geometry'):
+        if na not in ["null", "drop", "keep"]:
+            raise ValueError("Unknown na method {0}".format(na))
+        if col not in self._geometry_column_name:
+            raise ValueError("{} is not a geometry column".format(col))
+        ids = np.array(self.index, copy=False)
+        geometries = self[col].as_geojson()
+        geometries_bbox = self[col].envelope
+        print(geometries_bbox)
+
+        propertries_cols = self.columns.difference([col])
+        print(propertries_cols)
+
+        if len(propertries_cols) > 0:
+            properties = self[propertries_cols].astype(object).values
+            if na == "null":
+                properties[pd.isnull(self[propertries_cols]).values] = None
+
+            for i, row in enumerate(properties):
+                geom = geometries[i]
+
+                if na == "drop":
+                    propertries_items = {
+                        k: v for k, v in zip(propertries_cols, row) if not pd.isnull(v)
+                    }
+                else:
+                    propertries_items = {k: v for k, v in zip(propertries_cols, row)}
+
+                feature = {
+                    "id": str(ids[i]),
+                    "type": "Feature",
+                    "properties": propertries_items,
+                    "geometry": geom
+                }
+
+                if show_bbox:
+                    feature["bbox"] = geometries_bbox[i]
+                yield feature
+
+        else:
+            for fid, geom, bbox in zip(ids, geometries, geometries_bbox):
+                feature = {
+                    "id": str(fid),
+                    "type": "Feature",
+                    "properties": {},
+                    "geometry": geom,
+                }
+                if show_bbox:
+                    feature["bbox"] = bbox if geom else None
+                yield feature
+
     def to_geopandas(self):
         import geopandas
+        copy_df = self.copy()
         if self._geometry_column_name is not None:
-            for col in self._geometry_column_name:
-                self[col] = Series(self[col].to_geopandas())
-        return geopandas.GeoDataFrame(self.values ,columns=self.columns.values.tolist())
+            for col in copy_df._geometry_column_name:
+                copy_df[col] = Series(copy_df[col].to_geopandas())
+        return geopandas.GeoDataFrame(copy_df.values , columns=copy_df.columns.values.tolist())
 
     @classmethod
     def from_geopandas(cls, pdf):
@@ -143,13 +211,27 @@ class GeoDataFrame(DataFrame):
 
     def merge(self, *args, **kwargs):
         result = DataFrame.merge(self, *args, **kwargs)
-        geo_col = self._geometry_column_name
-        if isinstance(result, DataFrame) and geo_col in result:
+        geo_cols = self._geometry_column_name
+        geos_crs = {}
+        for col in geo_cols:
+            geos_crs[col] = self[col].crs
+        result_cols = result.columns.values.tolist()
+        flag = True
+        for col in geo_cols:
+            if col in result_cols:
+                flag = False
+
+        if isinstance(result, DataFrame) and not flag:
             result.__class__ = GeoDataFrame
-            result.crs = self.crs
-            result._geometry_column_name = geo_col
+            for col in geo_cols:
+                if col in result_cols:
+                    result[col] = GeoSeries(result[col])
+            for col in geo_cols:
+                if col in result_cols:
+                    result[col].set_crs(geos_crs[col])
+                    result._geometry_column_name.append(col)
             result._invalidate_sindex()
-        elif isinstance(result, DataFrame) and geo_col not in result:
+        elif isinstance(result, DataFrame) and flag:
             result.__class__ = DataFrame
         return result
 
