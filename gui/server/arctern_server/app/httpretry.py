2fc4e7af3988971dd01ae903a6da5f5bda47b37f
DragonDriver <31589260+DragonDriver@users.noreply.github.com>
2020-07-04 14:41:13 +0800

    adapt restful api & render function with spark 3.0.0 (#796)
    * adapt MAP_ITER UDF with Python type hints in spark3
    
    * adapt restful api with spark3
    
    * adapt restful api with spark3
    
    * adapt restful api with spark3
    
    * pylint check & format code

--- gui/server/arctern_server/app/httpretry.py
+++ gui/server/arctern_server/app/httpretry.py
@@ -1,12 +1,9 @@
 """
 Copyright (C) 2019-2020 Zilliz. All rights reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
      http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,36 +11,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from abc import ABCMeta, abstractmethod
+import requests
+from retrying import retry
 
-class DB(metaclass=ABCMeta):
-    def dbtype(self):
-        return self._db_type
-
-    def id(self):
-        return self._db_id
-
-    def name(self):
-        return self._db_name
-
-    @abstractmethod
-    def table_list(self):
-        return self._table_list
-
-    @abstractmethod
-    def run(self, sql):
-        pass
-
-    @abstractmethod
-    def load(self, metas):
-        pass
-
-    @abstractmethod
-    def run_for_json(self, sql):
-        pass
-
-    @abstractmethod
-    def get_table_info(self, table_name):
-        pass
-
-CENTER = {}
+@retry(stop_max_attempt_number=3, wait_fixed=3000)
+def safe_requests(*args, **kwargs):
+    return requests.request(*args, **kwargs)
