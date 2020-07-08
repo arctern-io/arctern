9d519164289ebbe187fd6136cfdd46b89d127f5d
czs007 <czs@alumni.hust.edu.cn>
2020-07-08 15:08:47 +0800

    pylint

--- python/arctern/geoseries/geoseries.py
+++ python/arctern/geoseries/geoseries.py
@@ -15,7 +15,7 @@
 # pylint: disable=useless-super-delegation
 # pylint: disable=too-many-lines
 # pylint: disable=too-many-public-methods
-# pylint: disable=too-many-ancestors,protected-access,too-many-branches,unidiomatic-typecheck,signature-differs
+# pylint: disable=too-many-ancestors,protected-access,too-many-branches,unidiomatic-typecheck,signature-differs,attribute-defined-outside-init
 
 from warnings import warn
 
