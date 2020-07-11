784cfa929e31ad67302394e1aa7c8e14d9f95e7e
bigsheeper <42060877+bigsheeper@users.noreply.github.com>
2020-07-11 10:32:20 +0800

    Length of polygon and multipolygon return 0  (#832)
    * polygon and multipolygon return 0 length
    
    * fix test

--- scala/src/main/scala/org/apache/spark/sql/arctern/expressions/Functions.scala
+++ scala/src/main/scala/org/apache/spark/sql/arctern/expressions/Functions.scala
@@ -28,6 +28,22 @@ import org.geotools.referencing.CRS
 import org.locationtech.jts.geom._
 
 object utils {
+  def length(geom: Geometry): Double = {
+    val geoType = geom.getGeometryType
+    if (geoType == "Polygon" || geoType == "MultiPolygon") return 0
+    if (geoType == "GeometryCollection") {
+      var lenSum: Double = 0
+      val geometries = geom.asInstanceOf[GeometryCollection]
+      for (i <- 0 until geometries.getNumGeometries) {
+        val g = geometries.getGeometryN(i)
+        val gType = g.getGeometryType
+        if (gType != "Polygon" && gType != "MultiPolygon") lenSum += g.getLength
+      }
+      return lenSum
+    }
+    geom.getLength
+  }
+
   def envelopeAsList(geom: Geometry): ArrayData = {
     if (geom == null || geom.isEmpty) {
       val negInf = scala.Double.NegativeInfinity
@@ -504,7 +520,7 @@ case class ST_Length(inputsExpr: Seq[Expression]) extends ST_UnaryOp {
 
   override def expr: Expression = inputsExpr.head
 
-  override protected def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = codeGenJob(ctx, ev, geo => s"$geo.getLength()")
+  override protected def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = codeGenJob(ctx, ev, geo => s"org.apache.spark.sql.arctern.expressions.utils.length($geo)")
 
   override def dataType: DataType = DoubleType
 
