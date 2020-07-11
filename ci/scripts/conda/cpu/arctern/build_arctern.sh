211c72eaa2948ff3bd5c54ec42d0dbef454be94b
quicksilver <zhifeng.zhang@zilliz.com>
2020-07-11 16:18:06 +0800

    conda build script file
    Signed-off-by: quicksilver <zhifeng.zhang@zilliz.com>

--- ci/scripts/conda/cpu/arctern/build_arctern.sh
+++ ci/scripts/conda/cpu/arctern/build_arctern.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+CONDA_PY=${CONDA_PY:="37"}
+
 if [ "$BUILD_ARCTERN" == '1' ]; then
   echo "Building arctern..."
-  conda build conda/recipes/arctern/cpu -c defaults -c conda-forge
+  CONDA_PY="${CONDA_PY}" conda build conda/recipes/arctern/cpu -c defaults -c conda-forge
 fi
--- ci/scripts/conda/gpu/arctern/build_arctern.sh
+++ ci/scripts/conda/gpu/arctern/build_arctern.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+CONDA_PY=${CONDA_PY:="37"}
+
 if [ "$BUILD_ARCTERN" == '1' ]; then
   echo "Building arctern..."
-  conda build conda/recipes/arctern/gpu -c defaults -c conda-forge -c nvidia
+  CONDA_PY="${CONDA_PY}" conda build conda/recipes/arctern/gpu -c defaults -c conda-forge -c nvidia
 fi
