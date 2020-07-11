f73244a49efd879f96f30c51b5225967c03759d0
beautifulpython <2426009680@qq.com>
2020-07-11 17:42:59 +0800

    chmod

--- tests/koalas_test/spark_koalas_regression.py
+++ tests/koalas_test/spark_koalas_regression.py
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import os
-from ogr import Geometry
-from ogr import CreateGeometryFromWkt
 from shapely import wkt
 
 GEO_TYPES = ['POLYGON', 'POINT', 'LINESTRING', 'LINEARRING']
@@ -127,130 +126,6 @@ def is_geometrycollection(geo):
         if geo.startswith(x):
             return True
         continue
-
-    return False
-
-
-def line_to_points(geom):
-    if geom.strip().upper().startswith('LINESTRING'):
-        points = geom.split('(')[1].split(')')[0]
-        arr = points.split(',')
-        xs = ['POINT (%s)' % x.strip() for x in arr]
-        return xs
-    return None
-
-
-def polygon_to_points(geom):
-    if geom.strip().upper().startswith('POLYGON'):
-        points = geom.split('((')[1].split('))')[0]
-        arr = points.split(',')
-        xs = ['POINT (%s)' % x.strip().replace(
-            '(', '').replace(')', '') for x in arr]
-        return xs
-    return None
-
-
-def geometrycollection_tolist(geom):
-    if is_geometrycollection(geom):
-        gc = wkt.loads(geom)
-        return [x.to_wkt() for x in list(gc)]
-    return None
-
-
-def point_distance(geox, geoy):
-    if is_geometry(geoy) and geox.strip().upper().startswith('POINT'):
-        p = wkt.loads(geox)
-        g = wkt.loads(geoy)
-        return p.distance(g)
-    return None
-
-
-def linestring_distance(geox, geoy):
-    if is_geometry(geoy) and geox.strip().upper().startswith('LINESTRING'):
-        xs = line_to_points(geox)
-        distance_arr = [point_distance(x, geoy) for x in xs]
-
-        return max(distance_arr)
-    return None
-
-
-def polygon_distance(geox, geoy):
-    if is_geometry(geoy) and geox.strip().upper().startswith('POLYGON'):
-        xs = polygon_to_points(geox)
-        distance_arr = [point_distance(x, geoy) for x in xs]
-
-        return max(distance_arr)
-    return None
-
-
-def geometry_distance(geox, geoy):
-    if is_point(geox):
-        d = point_distance(geox, geoy)
-
-    if is_linestring(geox):
-        d = linestring_distance(geox, geoy)
-
-    if is_polygon(geox):
-        d = polygon_distance(geox, geoy)
-
-    return d
-
-
-def arc_distance(geox, geoy):
-    if is_empty(geox) or is_empty(geoy):
-        return 0.0
-
-    if is_geometrycollection(geox) and is_geometrycollection(geoy):
-        gcx = geometrycollection_tolist(geox)
-        gcy = geometrycollection_tolist(geoy)
-
-        arr = []
-        for gx in gcx:
-            distance_arr = [geometry_distance(gx, gy) for gy in gcy]
-            arr.append(min(distance_arr))
-
-        return max(arr)
-
-    if is_geometry(geox) and is_geometrycollection(geoy):
-        gc = geometrycollection_tolist(geoy)
-        distance_arr = [geometry_distance(geox, x) for x in gc]
-
-        return max(distance_arr)
-
-    if is_geometry(geoy) and is_geometrycollection(geox):
-        return arc_distance(geoy, geox)  # pylint: disable=arguments-out-of-order
-
-    if is_geometry(geox) and is_geometry(geoy):
-        return geometry_distance(geox, geoy)
-
-    return None
-
-
-def is_length_types(geo):
-    """Determine whether a geometry is point/linestring/multipoint/multilinestring."""
-    geo = geo.strip().upper()
-
-    for a_geo_type_in_geo_length_types_list in GEO_LENGTH_TYPES:
-        if geo.startswith(a_geo_type_in_geo_length_types_list) and \
-                len(geo) != len(a_geo_type_in_geo_length_types_list):
-            return True
-
-        continue
-
-    return False
-
-
-def is_area_types(geo):
-    """Determine whether a geometry is polygon/multipolygon."""
-    geo = geo.strip().upper()
-
-    for a_geo_type_in_geo_area_types_list in GEO_AREA_TYPES:
-        if geo.startswith(a_geo_type_in_geo_area_types_list) and \
-                len(geo) != len(a_geo_type_in_geo_area_types_list):
-            return True
-
-        continue
-
     return False
 
 
@@ -307,21 +182,21 @@ EPOCH_CURVE_RELATIVE = 1e-2
 EPOCH_SURFACE_RELATIVE = 1e-2
 
 
-def compare_length(geometry_x, geometry_y):
-    """Compare length of 2 geometry types."""
-    arct = CreateGeometryFromWkt(geometry_x)
-    pgis = CreateGeometryFromWkt(geometry_y)
-
-    intersection_length = Geometry.Length(Geometry.Intersection(arct, pgis))
-    arct_length = Geometry.Length(arct)
-    pgis_length = Geometry.Length(pgis)
-
-    # print('arctern length: %s, postgis length: %s, intersection length: %s' %
-    #       (str(arct_length), str(pgis_length), str(intersection_length)))
-    # result = compare_float(intersection_length, arct_length, pgis_length, EPOCH_CURVE)
-    result = compare3float_relative(pgis_length, arct_length,
-                                    intersection_length, EPOCH_CURVE_RELATIVE)
-    return result
+# def compare_length(geometry_x, geometry_y):
+#     """Compare length of 2 geometry types."""
+#     arct = CreateGeometryFromWkt(geometry_x)
+#     pgis = CreateGeometryFromWkt(geometry_y)
+#
+#     intersection_length = Geometry.Length(Geometry.Intersection(arct, pgis))
+#     arct_length = Geometry.Length(arct)
+#     pgis_length = Geometry.Length(pgis)
+#
+#     # print('arctern length: %s, postgis length: %s, intersection length: %s' %
+#     #       (str(arct_length), str(pgis_length), str(intersection_length)))
+#     # result = compare_float(intersection_length, arct_length, pgis_length, EPOCH_CURVE)
+#     result = compare3float_relative(pgis_length, arct_length,
+#                                     intersection_length, EPOCH_CURVE_RELATIVE)
+#     return result
 
 
 def compare_area(geometry_x, geometry_y):
@@ -605,7 +480,7 @@ def update_result():
 #
 # import from compare.py ,These codes need to be refactored later.
 import pandas as pd
-from osgeo import ogr
+# from osgeo import ogr
 from arctern_spark.geoseries import GeoSeries
 from databricks.koalas import Series
 
