7ed5ef1f8ad7f7d3e71a3ccee35c04932106b36a
DragonDriver <longjiquan1998@163.com>
2020-07-15 10:30:32 +0800

    fix pyspark install bug

--- ci/jenkins/step/arcternSparkRegression.groovy
+++ ci/jenkins/step/arcternSparkRegression.groovy
@@ -22,9 +22,6 @@ try {
         // }
     }
 } catch(exc) {
-    dir ("docker/test_env") {
-        sh "docker-compose -p ${composeProject} logs regression"
-    }
     dir ("docker/test_env/spark/${BINARY_VERSION}") {
         sh "docker-compose -p ${composeProject} logs spark-master"
         sh "docker-compose -p ${composeProject} logs spark-worker"
