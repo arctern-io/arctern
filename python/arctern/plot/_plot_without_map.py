8f6180d5927139619064f25d0e365bc1b528595d
bigsheeper <42060877+bigsheeper@users.noreply.github.com>
2020-07-16 15:36:17 +0800

    add test in comment

--- python/arctern/plot/_plot_without_map.py
+++ python/arctern/plot/_plot_without_map.py
@@ -462,7 +462,7 @@ def fishnetmap_layer(w, h, points, weights, bounding_box,
        >>> points = arctern.GeoSeries.point(df['longitude'], df['latitude']) # doctest: +SKIP
        >>>
        >>> # render fishnet layer
-       >>> bbox = [-74.01424568752932, 40.72759334104623, -73.96056823889673, 40.76721122683304]
+       >>> bbox = [-74.01424568752932, 40.72759334104623, -73.96056823889673, 40.76721122683304] # doctest: +SKIP
        >>> map_layer = arctern.plot.fishnetmap_layer(1024, 896, points=points, weights=df['color_weights'], bounding_box=bbox, cell_size=8, cell_spacing=2, opacity=1.0, coordinate_system="EPSG:4326") # doctest: +SKIP
        >>> fig, ax = plt.subplots(figsize=(10, 6), dpi=200) # doctest: +SKIP
        >>> f = io.BytesIO(base64.b64decode(map_layer)) # doctest: +SKIP
@@ -518,6 +518,7 @@ def unique_value_choropleth_map_layer(w, h, region_boundaries, labels, bounding_
        >>> import pandas as pd
        >>> import numpy as np
        >>> import arctern
+       >>> import random
        >>> import matplotlib.pyplot as plt
        >>> import io
        >>> import base64
@@ -530,8 +531,22 @@ def unique_value_choropleth_map_layer(w, h, region_boundaries, labels, bounding_
        >>> input = df[pd.notna(df['region_boundaries'])].groupby(['region_boundaries']).mean().reset_index()
        >>> polygon = arctern.GeoSeries(input['region_boundaries'])
        >>>
+       >>> value_data = []
+       >>> for i in range(len(polygon)):
+       >>>   value_data.append(random.randint(0, 6))
+       >>>
+       >>> values = pd.Series(value_data)
+       >>>
        >>> # Plot choroplethmap layer
-       >>> bbox = [-74.01124953254566,40.73413446570038,-73.96238859103838,40.766161712662296]
+       >>> bbox = [-74.01124953254566,40.73413446570038,-73.96238859103838,40.766161712662296] # doctest: +SKIP
+       >>> unique_value_infos = {1: "#FF0000", 2: "#00FF00", 3: "#0000FF", 4: "#00FFFF", 5: "#FF0000"} # doctest: +SKIP
+       >>> map_layer = arctern.plot.unique_value_choropleth_map_layer(1024, 896, polygon, values, bounding_box=bbox, unique_value_infos=unique_value_infos, opacity=1.0, coordinate_system='EPSG:4326') # doctest: +SKIP
+       >>> fig, ax = plt.subplots(figsize=(10, 6), dpi=200) # doctest: +SKIP
+       >>> f = io.BytesIO(base64.b64decode(map_layer)) # doctest: +SKIP
+       >>> img = plt.imread(f) # doctest: +SKIP
+       >>> ax.imshow(img) # doctest: +SKIP
+       >>> ax.axis('off') # doctest: +SKIP
+       >>> plt.show() # doctest: +SKIP
     """
     vega = vega_unique_value_choroplethmap(w, h, bounding_box=bounding_box, unique_value_infos=unique_value_infos,
                               opacity=opacity, coordinate_system=coordinate_system)
